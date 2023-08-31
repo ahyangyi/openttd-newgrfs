8fd87213f1fb18cee53fa2e61c79fdce792e74bc
Yi Yang @ Anteros <ahyangyi@gmail.com>
2023-08-31 22:04:06 +0800

    Change persistent API

--- road/lib/__init__.py
+++ road/lib/__init__.py
@@ -12,7 +12,7 @@ class ARoadType(grf.SpriteGenerator):
 
     def get_sprites(self, g):
         if self.toolbar_caption and "toolbar_caption" not in self._props:
-            self._props["toolbar_caption"] = g.strings.add(self.toolbar_caption).get_persistent_id(grf.ROADTYPE)
+            self._props["toolbar_caption"] = g.strings.add(self.toolbar_caption).get_persistent_id()
 
         res = []
 
