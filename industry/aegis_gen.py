a145ba408c778c934a2b3757bd58c78e5eaa5c38
Yi Yang @ Anteros <ahyangyi@gmail.com>
2023-09-01 02:42:22 +0800

    Translate more mod names

--- industry/aegis_gen.py
+++ industry/aegis_gen.py
@@ -3,10 +3,13 @@ import grf
 
 
 def main():
+    s = grf.StringManager()
+    s.import_lang_dir("industry/lang", default_lang_file="english-uk.lng")
+
     g = grf.NewGRF(
         grfid=b"\xE5\xBC\x8Bi",
-        name="Ahyangyi's Extended Generic Industry Set",
-        description="Industry sets with options to support various playstyles.",
+        name=s["STR_GRF_NAME"],
+        description=s["STR_GRF_DESC"],
         id_map_file="industry/id_map.json",
     )
 
--- road/dovemere_gen.py
+++ road/dovemere_gen.py
@@ -3,10 +3,13 @@ import grf
 
 
 def main():
+    s = grf.StringManager()
+    s.import_lang_dir("road/lang", default_lang_file="english-uk.lng")
+
     g = grf.NewGRF(
         grfid=b"\xE5\xBC\x8Br",
-        name="Ahyangyi's Dovemere Road Set",
-        description="China-inspired road set.",
+        name=s["STR_GRF_NAME"],
+        description=s["STR_GRF_DESC"],
         id_map_file="road/id_map.json",
         sprite_cache_path="road/.cache",
     )
