708fb15368cef1bf86b0bc6e51cd0aa89d7c9b06
Yi Yang @ Aquamarine <ahyangyi@gmail.com>
2023-09-01 16:57:13 +0800

    Add yellowriver

--- road_vehicle/rosters/dovemere.py
+++ road_vehicle/rosters/dovemere.py
@@ -8,7 +8,7 @@ from ..vehicles.buses import (
     raregem,
     milkyway_2decker,
 )
-from ..vehicles.lorries import freedom, freedom_mkii, shield, happyone as happyone_truck
+from ..vehicles.lorries import freedom, freedom_mkii, shield, happyone as happyone_truck, yellowriver
 from ..vehicles.debug import visible_serpent
 from ..vehicles.monorail import nova
 from road_vehicle.lib.roster import Roster
@@ -26,6 +26,7 @@ roster = Roster(
     freedom,
     freedom_mkii,
     shield,
+    yellowriver,
     nova,
     visible_serpent,
 )
---
+++ road_vehicle/vehicles/lorries/yellowriver.py
@@ -0,0 +1,59 @@
+from datetime import date
+from road_vehicle.lib import ALorry, BiasPlyTire
+from road_vehicle.lib.graphics.autowolf import AutoWolf
+from road_vehicle.lib.graphics.voxel import LazyVoxel, LazyAlternatives, LazySwitch
+from agrf.variant import AVariant
+import cargos
+from agrf.graphics.recolour import *
+
+lowside = LazyVoxel(
+    "freedom",
+).compose(
+    "road_vehicle/voxels/parts/open.vox",
+    "open",
+)
+
+empty = lowside.produce_empty("empty")
+coal = lowside.compose(
+    "road_vehicle/voxels/parts/cargo/coal.vox",
+    "coal",
+)
+
+lowside_switch = LazySwitch(
+    ranges={
+        cargos.cargos.index(k): LazyAlternatives(
+            (
+                empty,
+                lowside.compose(
+                    "road_vehicle/voxels/parts/cargo/coal.vox",
+                    v.name,
+                    colour_map=v,
+                ),
+            )
+        )
+        for k, v in cargos.coal_remaps.items()
+    },
+    default=LazyAlternatives((empty, coal)),
+    code="cargo_type_in_veh",
+)
+
+variant = AVariant(
+    id=0x2300,
+    name="Yellow River Truck",
+    translation_name="YELLOWRIVER",
+    introduction_date=date(1960, 4, 15),
+    vehicle_life=15,
+    model_life=30,
+    max_speed=ALorry.kmh(60),
+    power=ALorry.kW(103),
+    weight=5,  # NO Data
+    techclass="h_truck",
+    cargo_capacity=8,
+    default_cargo_type=0,
+    refittable_cargo_classes=cargos.OPEN_CARGO_CLASSES,
+    real_class=ALorry,
+    graphics_helper=AutoWolf(
+        lowside_switch,
+        flags=("noflipY",),
+    ),
+)
