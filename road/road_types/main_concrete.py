131170937ca26e6231ae8fdc359ae59b26757caf
Yi Yang @ Anteros <ahyangyi@gmail.com>
2023-09-06 13:45:08 +0800

    New roads

---
+++ road/road_types/main_concrete.py
@@ -0,0 +1,14 @@
+from road.lib import ARoadType
+from road.lib.graphics.underlay import get_spritesheet
+from datetime import date
+import grf
+
+
+the_road = ARoadType(
+    id=0x05,
+    name="Main Road (Concrete)",
+    label=b"CON3",
+    introduction_date=date(1940, 1, 1),
+    underlay=get_spritesheet("wolf_a"),
+    translation_name="MAIN_CONCRETE",
+)
---
+++ road/road_types/urban_expressway_asphalt.py
@@ -0,0 +1,14 @@
+from road.lib import ARoadType
+from road.lib.graphics.underlay import get_spritesheet
+from datetime import date
+import grf
+
+
+the_road = ARoadType(
+    id=0x05,
+    name="Urban Expressway (Asphalt)",
+    label=b"UREA",
+    introduction_date=date(1970, 1, 1),
+    underlay=get_spritesheet("wolf_a"),
+    translation_name="URBAN_EXPRESSWAY",
+)
