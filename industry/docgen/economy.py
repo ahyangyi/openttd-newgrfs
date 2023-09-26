96a4ed90e4fbbe46e4388b1051e303a0bc8fcbdb
Yi Yang @ Anteros <ahyangyi@gmail.com>
2023-09-27 04:58:51 +0800

    (Aegis) Add links

--- industry/docgen/economy.py
+++ industry/docgen/economy.py
@@ -22,6 +22,8 @@ nav_order: {i+1}
             )
             translate = lambda x: get_translation(string_manager["STR_CARGO_" + x.decode()], 0x7F)
             for industry, flow in entry.graph.items():
-                accepts = ", ".join(translate(x.label) for x in flow.accepts)
+                accepts = ", ".join(
+                    f"[{translate(x.label)}](/openttd-newgrfs/industry/cargos/{x.label}.html)" for x in flow.accepts
+                )
                 produces = ", ".join(translate(x.label) for x in flow.produces)
                 print(f"| {industry.name} | {accepts} | {produces} |", file=f)
