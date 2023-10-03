c4b1563650a306eda0266391c5942c3abddf082c
Yi Yang @ Anteros <ahyangyi@gmail.com>
2023-10-03 21:22:38 +0800

    (Road-type) Add images to docs

--- road/lib/docgen.py
+++ road/lib/docgen.py
@@ -1,11 +1,15 @@
 import os
+import grf
 from agrf.strings import get_translation
 
 
 def gen_docs(string_manager, road_types):
     prefix = "docs/road/"
+    os.makedirs(os.path.join(prefix, "img"), exist_ok=True)
     for i, road_type in enumerate(road_types):
         translation = get_translation(string_manager[f"STR_RT_{road_type.translation_name}_NAME"], 0x7F)
+        img, bpp = road_type.underlay[0].get_zoom(grf.ZOOM_NORMAL).get_image()
+        img.save(os.path.join(prefix, "img", f"{road_type.translation_name}.png"))
         with open(os.path.join(prefix, f"{road_type.translation_name}.md"), "w") as f:
             print(
                 f"""---
@@ -14,7 +18,7 @@ title: {translation}
 parent: Ahyangyi's Chinese Road Set (ACRS)
 nav_order: {i+1}
 ---
-Blablabla
+![](img/{road_type.translation_name}.png)
 """,
                 file=f,
             )
