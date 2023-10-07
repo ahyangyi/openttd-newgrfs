911ad62596152805eeca839aceb0db25c9557f2f
Yi Yang @ Anteros <ahyangyi@gmail.com>
2023-10-08 01:14:08 +0800

    (RV) add a second roster

---
+++ road_vehicle/rosters/norbury.py
@@ -0,0 +1,17 @@
+from ..vehicles.buses import (
+    northwing,
+    milkyway_2decker,
+)
+from ..vehicles.lorries import freedom, freedom_mkii, shield, happyone as happyone_truck, yellowriver
+from road_vehicle.lib.roster import Roster
+
+roster = Roster(
+    northwing,
+    milkyway_2decker,
+    happyone_truck,
+    freedom,
+    freedom_mkii,
+    shield,
+    yellowriver,
+    nova,
+)
---
+++ road_vehicle/vehicles/buses/northwing.py
@@ -0,0 +1,25 @@
+from datetime import date
+from road_vehicle.lib import ABus, BiasPlyTire
+from road_vehicle.lib.graphics.autowolf import AutoWolf
+from agrf.variant import AVariant
+
+
+variant = AVariant(
+    real_class=ABus,
+    id=0x1060,
+    name="Northwing Bus",
+    translation_name="NORTHWING",
+    introduction_date=date(1957, 1, 1),
+    vehicle_life=15,
+    model_life=30,
+    max_speed=ABus.kmh(55),  # No data
+    power=ABus.hp(95),
+    weight=5.0,
+    techclass="bus",
+    cargo_capacity=80,
+    real_dimensions=(8.2, 2.5, 3.05),
+    real_x_dimensions=(0, 4.46, 0),
+    axle_track=(1.70, 1.74),  # No data, using CA10
+    tire=BiasPlyTire(9, 20),  # No data, using CA10
+    graphics_helper=AutoWolf("placeholder"),
+)
