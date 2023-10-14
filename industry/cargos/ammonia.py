c8aa14a41ffc1703ec4cb3c37992528ec9f224c2
Yi Yang @ Anteros <ahyangyi@gmail.com>
2023-10-15 00:20:00 +0800

    (Aegis) align weight with firs

--- industry/cargos/ammonia.py
+++ industry/cargos/ammonia.py
@@ -1,4 +1,4 @@
 import grf
 from industry.lib.cargo import ACargo
 
-the_cargo = ACargo(0x10, b"NH3_", grf.CargoClass.LIQUID | grf.CargoClass.HAZARDOUS)
+the_cargo = ACargo(0x10, b"NH3_", grf.CargoClass.LIQUID | grf.CargoClass.HAZARDOUS, weight=10)
