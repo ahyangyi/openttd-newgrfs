b16545a76377708f71d6418ca6f863bdc1fa53d6
Yi Yang @ Anteros <ahyangyi@gmail.com>
2023-10-25 02:49:19 +0800

    (Aegis) Make alcohol lighter

--- industry/cargos/alcohol.py
+++ industry/cargos/alcohol.py
@@ -2,5 +2,5 @@ import grf
 from industry.lib.cargo import ACargo
 
 the_cargo = ACargo(
-    0x39, b"BEER", grf.CargoClass.EXPRESS | grf.CargoClass.PIECE_GOODS | grf.CargoClass.LIQUID, weight=18
+    0x39, b"BEER", grf.CargoClass.EXPRESS | grf.CargoClass.PIECE_GOODS | grf.CargoClass.LIQUID, weight=17
 )
