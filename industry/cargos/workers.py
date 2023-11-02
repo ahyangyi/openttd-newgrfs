cd848d1fb4372a411f23ddb36cd1bce7d8ae4864
Yi Yang @ Aquamarine <ahyangyi@gmail.com>
2023-11-02 17:11:44 +0800

    (Aegis) worker change to "WKRK"; OTI1 is so weird.

--- industry/cargos/workers.py
+++ industry/cargos/workers.py
@@ -3,7 +3,7 @@ from industry.lib.cargo import ACargo, CargoUnit
 
 the_cargo = ACargo(
     0x33,
-    b"OTI1",
+    b"WRKR",
     grf.CargoClass.PASSENGERS,
     capacity_multiplier=0x400,
     weight=1,
