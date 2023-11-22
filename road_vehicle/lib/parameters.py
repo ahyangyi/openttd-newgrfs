e865b659fc9945a24a84b50e6fef32d3fb9eeb1d
Yi Yang @ Anteros <ahyangyi@gmail.com>
2023-11-23 04:40:42 +0800

    (RV) More options

--- road_vehicle/lib/parameters.py
+++ road_vehicle/lib/parameters.py
@@ -12,5 +12,23 @@ parameter_list = ParameterList(
             },
         ),
         Parameter("NIGHT_MODE", 0, {0: "AUTO_DETECT", 1: "ENABLED", 2: "DISABLED"}),
+        # FIXME: change the order after migrating the code
+        Parameter(
+            "ROSTER",
+            0,
+            {
+                0: "ALL",
+                1: "DOVEMERE",
+                2: "NORBURY",
+            },
+        ),
+        Parameter(
+            "EARLY_IMPORTED_VEHICLES",
+            1,
+            {
+                0: "DISABLED",
+                1: "ENABLED",
+            },
+        ),
     ]
 )
