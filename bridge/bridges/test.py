56273c9d0c8a31d29c71b422c5d8e4fbeebf7f2d
Yi Yang @ Aquamarine <ahyangyi@gmail.com>
2023-11-23 21:10:44 +0800

    (bridge) add placeholder voxels

--- bridge/bridges/test.py
+++ bridge/bridges/test.py
@@ -2,6 +2,16 @@ from bridge.lib import ABridge
 from datetime import date
 import grf
 
+from agrf.graphics.voxel import LazyVoxel, LazySpriteSheet
+
+vox = LazyVoxel(
+    "dovemere_yangtze_1",
+    prefix=f"bridge/voxels/render/dovemere_yangtze_1",
+    voxel_getter=lambda: f"bridge/voxels/dovemere_yangtze_1.vox",
+    load_from="bridge/files/gorender.json",
+)
+vox.render()
+voxels = [LazySpriteSheet([vox], [(0, 0)])]
 
 the_bridge = ABridge(
     id=0x01,
