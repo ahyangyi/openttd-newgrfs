c679d7f9f9f22ea04b8b3f90b45a307bfcc71148
Yi Yang @ Aquamarine <ahyangyi@gmail.com>
2024-02-18 22:00:58 +0800

    (rt) fix preview images zoom

--- road/lib/docgen.py
+++ road/lib/docgen.py
@@ -8,7 +8,7 @@ def gen_docs(string_manager, road_types):
     os.makedirs(os.path.join(prefix, "img"), exist_ok=True)
     for i, road_type in enumerate(road_types):
         translation = get_translation(string_manager[f"STR_RT_{road_type.translation_name}_NAME"], 0x7F)
-        img, bpp = road_type.underlay[0].get_sprite(zoom=grf.ZOOM_NORMAL, bpp=32).sprite.get_image()
+        img, bpp = road_type.underlay[0].get_sprite(zoom=grf.ZOOM_4X, bpp=32).sprite.get_image()
         img.save(os.path.join(prefix, "img", f"{road_type.translation_name}.png"))
         with open(os.path.join(prefix, f"{road_type.translation_name}.md"), "w") as f:
             print(
