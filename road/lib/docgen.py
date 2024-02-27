0395c32ada537e2c9125cfef73ebfaec80e40fce
Yi Yang @ Aquamarine <ahyangyi@gmail.com>
2024-02-27 21:09:14 +0800

    (rt) display overlay when available

--- road/lib/docgen.py
+++ road/lib/docgen.py
@@ -8,8 +8,6 @@ def gen_docs(string_manager, road_types):
     os.makedirs(os.path.join(prefix, "img"), exist_ok=True)
     for i, road_type in enumerate(road_types):
         translation = get_translation(string_manager[f"STR_RT_{road_type.translation_name}_NAME"], 0x7F)
-        img, bpp = road_type.underlay[0].get_sprite(zoom=grf.ZOOM_4X, bpp=32).sprite.get_image()
-        img.save(os.path.join(prefix, "img", f"{road_type.translation_name}.png"))
         with open(os.path.join(prefix, f"{road_type.translation_name}.md"), "w") as f:
             print(
                 f"""---
@@ -18,7 +16,13 @@ title: {translation}
 parent: ACRS - Ahyangyi's Chinese Road Set
 nav_order: {i+1}
 ---
-![](img/{road_type.translation_name}.png)
 """,
                 file=f,
             )
+
+            for layer in ["underlay", "overlay"]:
+                if getattr(road_type, layer) is None:
+                    continue
+                img, bpp = getattr(road_type, layer)[0].get_sprite(zoom=grf.ZOOM_4X, bpp=32).sprite.get_image()
+                img.save(os.path.join(prefix, "img", f"{road_type.translation_name}_{layer}.png"))
+                print(f"{layer} ![](img/{road_type.translation_name}_{layer}.png)", file=f)
