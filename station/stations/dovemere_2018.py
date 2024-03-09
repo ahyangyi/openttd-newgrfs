d70a11a2078ab3de53c902e23836ab714db9c94c
Yi Yang @ Anteros <ahyangyi@gmail.com>
2024-03-10 06:24:15 +0800

    (station) more models, more automagicness

--- station/stations/dovemere_2018.py
+++ station/stations/dovemere_2018.py
@@ -15,35 +15,168 @@ front_gate = LazyVoxel(
     voxel_getter=lambda: "station/voxels/dovemere_2018/front_gate.vox",
     load_from="station/files/gorender.json",
 )
+front_gate_extender = LazyVoxel(
+    "front_gate_extender",
+    prefix="station/voxels/render/dovemere_2018",
+    voxel_getter=lambda: "station/voxels/dovemere_2018/front_gate_extender.vox",
+    load_from="station/files/gorender.json",
+)
 central = LazyVoxel(
     "central",
     prefix="station/voxels/render/dovemere_2018",
     voxel_getter=lambda: "station/voxels/dovemere_2018/central.vox",
     load_from="station/files/gorender.json",
 )
+central_windowed = LazyVoxel(
+    "central_windowed",
+    prefix="station/voxels/render/dovemere_2018",
+    voxel_getter=lambda: "station/voxels/dovemere_2018/central_windowed.vox",
+    load_from="station/files/gorender.json",
+)
+central_windowed_extender = LazyVoxel(
+    "central_windowed_extender",
+    prefix="station/voxels/render/dovemere_2018",
+    voxel_getter=lambda: "station/voxels/dovemere_2018/central_windowed_extender.vox",
+    load_from="station/files/gorender.json",
+)
+corner = LazyVoxel(
+    "corner",
+    prefix="station/voxels/render/dovemere_2018",
+    voxel_getter=lambda: "station/voxels/dovemere_2018/corner.vox",
+    load_from="station/files/gorender.json",
+)
 front_gate_flipped = front_gate.flip("flip")
+central_windowed_flipped = central_windowed.flip("flip")
+corner_flipped = corner.flip("flip")
 front_normal.render()
 front_gate.render()
 front_gate_flipped.render()
+front_gate_extender.render()
 central.render()
+central_windowed.render()
+central_windowed_extender.render()
+central_windowed_flipped.render()
+corner.render()
+corner_flipped.render()
 
 front_normal = front_normal.spritesheet(0, 0)
 front_gate = front_gate.spritesheet(0, 0)
 front_gate_flipped = front_gate_flipped.spritesheet(0, 0)
+front_gate_extender = front_gate_extender.spritesheet(0, 0)
 central = central.spritesheet(0, 0)
+central_windowed = central_windowed.spritesheet(0, 0)
+central_windowed_extender = central_windowed_extender.spritesheet(0, 0)
+central_windowed_flipped = central_windowed_flipped.spritesheet(0, 0)
+corner = corner.spritesheet(0, 0)
+corner_flipped = corner_flipped.spritesheet(0, 0)
 
-sprites = front_normal[2:] + front_normal + central[:2] + front_gate[2:] + front_gate_flipped[:2]
+sprites = (front_normal[2:] + front_normal + central[:2]) + [
+    front_gate[2],
+    front_gate_flipped[1],
+    front_gate_flipped[0],
+    front_gate[3],
+    corner[0],
+    corner_flipped[3],
+    corner[2],
+    corner_flipped[1],
+    corner_flipped[0],
+    corner[3],
+    corner_flipped[2],
+    corner[1],
+    central_windowed[0],
+    central_windowed_flipped[3],
+    central_windowed_flipped[2],
+    central_windowed[1],
+    front_gate_extender[2],
+    front_gate_extender[3],
+    central_windowed_extender[0],
+    central_windowed_extender[1],
+]
+
+
+def get_back_index(l, r):
+    if l + r <= 2:
+        # FIXME
+        return 2
+
+    e = l + r - 3
+    c = e // 3
+    if c % 2 != e % 2:
+        c += 1
+    o = (e - c) // 2
+    return ([18] + [2] * o + [2] + [2] * c + [2] + [2] * o + [12])[l]
+
+
+def get_central_index(l, r):
+    if l + r <= 2:
+        # FIXME
+        return 6
+
+    e = l + r - 3
+    c = e // 3
+    if c % 2 != e % 2:
+        c += 1
+    o = (e - c) // 2
+    return ([6] + [6] * o + [22] + [26] * c + [20] + [6] * o + [6])[l]
+
+
+def get_front_index(l, r):
+    if l + r == 0:
+        # FIXME
+        return 4
+    if l + r == 1:
+        return [14, 16][l]
+    if l + r == 2:
+        # FIXME
+        return [14, 4, 16][l]
+
+    e = l + r - 3
+    c = e // 3
+    if c % 2 != e % 2:
+        c += 1
+    o = (e - c) // 2
+    return ([14] + [4] * o + [8] + [24] * c + [10] + [4] * o + [16])[l]
+
 
 cb41 = grf.Switch(
     ranges={
         (0, 1): 0,
-        (2, 3): 2,
-        (4, 5): grf.Switch(
-            ranges={1: 10, 2: 8},
-            default=4,
-            code="var(0x41, shift=0, and=0x0000000f)",
+        (2, 3): grf.Switch(
+            ranges={
+                l: grf.Switch(
+                    ranges={r: get_back_index(l, r) for r in range(16)},
+                    default=2,
+                    code="var(0x41, shift=0, and=0x0000000f)",
+                )
+                for l in range(16)
+            },
+            default=2,
+            code="var(0x41, shift=4, and=0x0000000f)",
         ),
-        (6, 7): 6,
+        (4, 5): grf.Switch(
+            ranges={
+                l: grf.Switch(
+                    ranges={r: get_front_index(l, r) for r in range(16)},
+                    default=4,
+                    code="var(0x41, shift=0, and=0x0000000f)",
+                )
+                for l in range(16)
+            },
+            default=4,
+            code="var(0x41, shift=4, and=0x0000000f)",
+        ),
+        (6, 7): grf.Switch(
+            ranges={
+                l: grf.Switch(
+                    ranges={r: get_central_index(l, r) for r in range(16)},
+                    default=6,
+                    code="var(0x41, shift=0, and=0x0000000f)",
+                )
+                for l in range(16)
+            },
+            default=6,
+            code="var(0x41, shift=4, and=0x0000000f)",
+        ),
     },
     default=0,
     code="var(0x41, shift=24, and=0x0000000f)",
