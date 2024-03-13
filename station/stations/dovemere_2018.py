fa03bca813f9d05953e46ae90cb983db53548279
Yi Yang @ Anteros <ahyangyi@gmail.com>
2024-03-13 21:57:14 +0800

    (station) refactor

--- station/stations/dovemere_2018.py
+++ station/stations/dovemere_2018.py
@@ -3,131 +3,94 @@ from station.lib import AStation
 from agrf.graphics.voxel import LazyVoxel
 from agrf.sprites import number_alternatives
 
-front_normal = LazyVoxel(
-    "front_normal",
-    prefix="station/voxels/render/dovemere_2018",
-    voxel_getter=lambda: "station/voxels/dovemere_2018/front_normal.vox",
-    load_from="station/files/gorender.json",
-)
-front_gate = LazyVoxel(
-    "front_gate",
-    prefix="station/voxels/render/dovemere_2018",
-    voxel_getter=lambda: "station/voxels/dovemere_2018/front_gate.vox",
-    load_from="station/files/gorender.json",
-)
-front_gate_extender = LazyVoxel(
-    "front_gate_extender",
-    prefix="station/voxels/render/dovemere_2018",
-    voxel_getter=lambda: "station/voxels/dovemere_2018/front_gate_extender.vox",
-    load_from="station/files/gorender.json",
-)
-central = LazyVoxel(
-    "central",
-    prefix="station/voxels/render/dovemere_2018",
-    voxel_getter=lambda: "station/voxels/dovemere_2018/central.vox",
-    load_from="station/files/gorender.json",
-)
-central_windowed = LazyVoxel(
-    "central_windowed",
-    prefix="station/voxels/render/dovemere_2018",
-    voxel_getter=lambda: "station/voxels/dovemere_2018/central_windowed.vox",
-    load_from="station/files/gorender.json",
-)
-central_windowed_extender = LazyVoxel(
-    "central_windowed_extender",
-    prefix="station/voxels/render/dovemere_2018",
-    voxel_getter=lambda: "station/voxels/dovemere_2018/central_windowed_extender.vox",
-    load_from="station/files/gorender.json",
-)
-side_a = LazyVoxel(
-    "side_a",
-    prefix="station/voxels/render/dovemere_2018",
-    voxel_getter=lambda: "station/voxels/dovemere_2018/side_a.vox",
-    load_from="station/files/gorender.json",
-)
-corner = LazyVoxel(
-    "corner",
-    prefix="station/voxels/render/dovemere_2018",
-    voxel_getter=lambda: "station/voxels/dovemere_2018/corner.vox",
-    load_from="station/files/gorender.json",
-)
-front_gate_flipped = front_gate.flip("flip")
-central_windowed_flipped = central_windowed.flip("flip")
-side_a_flipped = side_a.flip("flip")
-corner_flipped = corner.flip("flip")
+front_normal, front_gate, front_gate_extender, central, central_windowed, central_windowed_extender, side_a, corner = [
+    LazyVoxel(
+        name,
+        prefix="station/voxels/render/dovemere_2018",
+        voxel_getter=lambda path=f"station/voxels/dovemere_2018/{name}.vox": path,
+        load_from="station/files/gorender.json",
+    )
+    for name in [
+        "front_normal",
+        "front_gate",
+        "front_gate_extender",
+        "central",
+        "central_windowed",
+        "central_windowed_extender",
+        "side_a",
+        "corner",
+    ]
+]
+
 front_normal.render()
 front_gate.render()
-front_gate_flipped.render()
 front_gate_extender.render()
 central.render()
 central_windowed.render()
 central_windowed_extender.render()
-central_windowed_flipped.render()
 side_a.render()
-side_a_flipped.render()
 corner.render()
-corner_flipped.render()
 
 front_normal = front_normal.spritesheet(0, 0)
 front_gate = front_gate.spritesheet(0, 0)
-front_gate_flipped = front_gate_flipped.spritesheet(0, 0)
 front_gate_extender = front_gate_extender.spritesheet(0, 0)
 central = central.spritesheet(0, 0)
 central_windowed = central_windowed.spritesheet(0, 0)
 central_windowed_extender = central_windowed_extender.spritesheet(0, 0)
-central_windowed_flipped = central_windowed_flipped.spritesheet(0, 0)
 side_a = side_a.spritesheet(0, 0)
-side_a_flipped = side_a_flipped.spritesheet(0, 0)
 corner = corner.spritesheet(0, 0)
-corner_flipped = corner_flipped.spritesheet(0, 0)
 
 sprites = [
     front_normal[0],
     front_normal[1],
-    front_normal[2],
-    front_normal[3],
+    front_normal[4],
+    front_normal[5],
     front_normal[0],
     front_normal[1],
     central[0],
     central[1],
     front_gate[0],
-    front_gate_flipped[3],
-    front_gate_flipped[2],
     front_gate[1],
-    corner[2],
-    corner_flipped[1],
+    front_gate[2],
+    front_gate[3],
     corner[0],
-    corner_flipped[3],
-    corner_flipped[2],
     corner[1],
-    corner_flipped[0],
+    corner[2],
     corner[3],
+    corner[4],
+    corner[5],
+    corner[6],
+    corner[7],
     central_windowed[2],
-    central_windowed_flipped[1],
-    central_windowed_flipped[0],
     central_windowed[3],
+    central_windowed[0],
+    central_windowed[1],
     front_gate_extender[0],
     front_gate_extender[1],
     central_windowed_extender[2],
     central_windowed_extender[3],
     side_a[0],
-    side_a_flipped[3],
-    side_a_flipped[2],
     side_a[1],
+    side_a[2],
+    side_a[3],
 ]
 
 
 def get_back_index(l, r):
-    if l + r <= 2:
+    if l + r == 0:
         # FIXME
         return 2
+    if l + r == 1:
+        return [18, 16][l]
+    if l + r == 2:
+        return [18, 2, 16][l]
 
     e = l + r - 3
     c = e // 3
     if c % 2 != e % 2:
         c += 1
     o = (e - c) // 2
-    return ([18] + [2] * o + [2] + [2] * c + [2] + [2] * o + [12])[l]
+    return ([18] + [2] * o + [2] + [2] * c + [2] + [2] * o + [16])[l]
 
 
 def get_central_index(l, r):
@@ -151,17 +114,17 @@ def get_front_index(l, r):
         # FIXME
         return 4
     if l + r == 1:
-        return [14, 16][l]
+        return [12, 14][l]
     if l + r == 2:
         # FIXME
-        return [14, 4, 16][l]
+        return [12, 4, 14][l]
 
     e = l + r - 3
     c = e // 3
     if c % 2 != e % 2:
         c += 1
     o = (e - c) // 2
-    return ([14] + [4] * o + [8] + [24] * c + [10] + [4] * o + [16])[l]
+    return ([12] + [4] * o + [8] + [24] * c + [10] + [4] * o + [14])[l]
 
 
 cb41 = grf.Switch(
