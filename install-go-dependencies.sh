e5733c4344c6ddd04fca153fa83875529ac86ba9
Yi Yang @ Anteros <ahyangyi@gmail.com>
2024-03-18 13:45:45 +0800

    (station) bump gorender again, soft_shadow does the trick!

--- install-go-dependencies.sh
+++ install-go-dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export GOPATH=$PWD/gopath
-go install github.com/ahyangyi/gorender/cmd@aff2cec9
+go install github.com/ahyangyi/gorender/cmd@f2e65476
 mv gopath/bin/cmd gopath/bin/gorender
 go install github.com/ahyangyi/cargopositor/cmd@latest
 mv gopath/bin/cmd gopath/bin/positor
