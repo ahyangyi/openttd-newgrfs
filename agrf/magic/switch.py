7d3d20bb1c64d7eab2f320d180ed134467052a39
Yi Yang @ Anteros <ahyangyi@gmail.com>
2024-03-22 23:15:06 +0800

    (general) add cache to magic switch

--- agrf/magic/switch.py
+++ agrf/magic/switch.py
@@ -1,17 +1,37 @@
 import grf
 
 
+def deep_freeze(thing):
+    from collections.abc import Collection, Mapping, Hashable
+    from frozendict import frozendict
+
+    if thing is None or isinstance(thing, str):
+        return thing
+    elif isinstance(thing, Mapping):
+        return frozendict({k: deep_freeze(v) for k, v in thing.items()})
+    elif isinstance(thing, Collection):
+        return tuple(deep_freeze(i) for i in thing)
+    elif not isinstance(thing, Hashable):
+        raise TypeError(f"unfreezable type: '{type(thing)}'")
+    else:
+        return thing
+
+
 class Switch(grf.Switch):
     def __init__(self, code, ranges, default, *, feature=None, ref_id=None, related_scope=False, subroutines=None):
         super().__init__(
             code, ranges, default, feature=feature, ref_id=ref_id, related_scope=related_scope, subroutines=subroutines
         )
+        self.attr_cache = {}
+        self.call_cache = {}
 
     def __getattr__(self, name):
+        if name in self.attr_cache:
+            return self.attr_cache[name]
         call = lambda x: getattr(x, name)
         new_ranges = {(r.low, r.high): call(r.ref) for r in self._ranges}
         new_default = call(self.default)
-        return Switch(
+        ret = Switch(
             self.code,
             new_ranges,
             new_default,
@@ -20,12 +40,17 @@ class Switch(grf.Switch):
             related_scope=self.related_scope,
             subroutines=self.subroutines,
         )
+        self.attr_cache[name] = ret
+        return ret
 
     def __call__(self, *args, **kwargs):
+        key = deep_freeze((args, kwargs))
+        if key in self.call_cache:
+            return self.call_cache[key]
         call = lambda x: x(*args, **kwargs)
         new_ranges = {(r.low, r.high): call(r.ref) for r in self._ranges}
         new_default = call(self.default)
-        return Switch(
+        ret = Switch(
             self.code,
             new_ranges,
             new_default,
@@ -34,3 +59,5 @@ class Switch(grf.Switch):
             related_scope=self.related_scope,
             subroutines=self.subroutines,
         )
+        self.call_cache[key] = ret
+        return ret
