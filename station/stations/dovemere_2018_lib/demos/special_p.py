44e5f932581fc629d403114e8b7da4a8a80e3fad
Yi Yang @ Anteros <ahyangyi@gmail.com>
2024-04-11 03:51:19 +0800

    ..

--- station/stations/dovemere_2018_lib/demos/special_p.py
+++ station/stations/dovemere_2018_lib/demos/special_p.py
@@ -9,7 +9,7 @@ special_demo_p = Demo(
         [None, h_end_gate_1.T, turn_gate.TR, None, turn_gate.T, h_end_gate_1.TR, None],
         [platform, platform, v_central, platform, v_central, platform, platform],
         [platform, platform, v_central, platform, v_central, platform, platform],
-        [h_end, h_windowed, junction4, h_windowed_extender, junction4, h_windowed.R, h_end.R],
+        [h_end, h_windowed, cross, h_windowed_extender, cross, h_windowed.R, h_end.R],
         [platform, platform, v_central, platform, v_central, platform, platform],
         [platform, platform, v_central, platform, v_central, platform, platform],
         [None, h_end_gate_1, turn_gate.R, None, turn_gate, h_end_gate_1.R, None],
