85f070ee4cd1222b3ffbf245fa17cc47c5d405a6
Yi Yang @ Anteros <ahyangyi@gmail.com>
2024-04-11 04:03:17 +0800

    ..

--- station/stations/dovemere_2018_lib/demos/special_g.py
+++ station/stations/dovemere_2018_lib/demos/special_g.py
@@ -8,11 +8,11 @@ rail_far = [x.T for x in rail_near]
 rail_both = [platform, v_central, platform, platform, platform, v_central, platform]
 station = [
     h_end_asym_platform,
-    tee,
+    tee_platform,
     h_gate_1_platform,
     h_gate_extender_1_platform,
     h_gate_1_platform.R,
-    tee,
+    tee_platform,
     h_end_asym_platform.R,
 ]
 
