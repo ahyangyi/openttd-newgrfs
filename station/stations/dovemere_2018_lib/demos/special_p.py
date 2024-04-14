206cba2fca7e8a153596b912e1f411efad8a6651
Yi Yang @ Anteros <ahyangyi@gmail.com>
2024-04-15 01:53:21 +0800

    ...

--- station/stations/dovemere_2018_lib/demos/special_p.py
+++ station/stations/dovemere_2018_lib/demos/special_p.py
@@ -6,13 +6,13 @@ from agrf.graphics.palette import CompanyColour
 special_demo_p = Demo(
     "Irregular 7×7 station layout",
     [
-        [None, h_end_gate_1.T, turn_gate.TR, None, turn_gate.T, h_end_gate_1.TR, None],
+        [None, h_end_asym_gate.T, turn_gate.TR, None, turn_gate.T, h_end_asym_gate.TR, None],
         [platform, platform, v_central, platform, v_central, platform, platform],
         [platform, platform, v_central, platform, v_central, platform, platform],
         [h_end, h_windowed, cross, h_windowed_extender, cross, h_windowed.R, h_end.R],
         [platform, platform, v_central, platform, v_central, platform, platform],
         [platform, platform, v_central, platform, v_central, platform, platform],
-        [None, h_end_gate_1, turn_gate.R, None, turn_gate, h_end_gate_1.R, None],
+        [None, h_end_asym_gate, turn_gate.R, None, turn_gate, h_end_asym_gate.R, None],
     ],
     remap=get_1cc_remap(CompanyColour.PALE_GREEN),
 )
