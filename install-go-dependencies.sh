316a5e0057ea68481a7cbe163743e8788cb9b6ae
Yi Yang @ Anteros <ahyangyi@gmail.com>
2024-04-16 07:49:07 +0800

    (station) Render the station slightly taller (+25%)

--- install-go-dependencies.sh
+++ install-go-dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export GOPATH=$PWD/gopath
-go install github.com/ahyangyi/gorender/cmd@f2e65476
+go install github.com/ahyangyi/gorender/cmd@daeb83b4
 mv gopath/bin/cmd gopath/bin/gorender
 go install github.com/ahyangyi/cargopositor/cmd@latest
 mv gopath/bin/cmd gopath/bin/positor
