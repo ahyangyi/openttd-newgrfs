3306be13bf032dc59e50b32da379c0bf8b5b2e85
Yi Yang @ Aquamarine <ahyangyi@gmail.com>
2024-04-19 17:58:08 +0800

    (station) Fix positor usage again -- with a patched positor this time

--- install-go-dependencies.sh
+++ install-go-dependencies.sh
@@ -2,7 +2,7 @@
 export GOPATH=$PWD/gopath
 go install github.com/ahyangyi/gorender/cmd@daeb83b4
 mv gopath/bin/cmd gopath/bin/gorender
-go install github.com/ahyangyi/cargopositor/cmd@latest
+go install github.com/ahyangyi/cargopositor/cmd@152f976
 mv gopath/bin/cmd gopath/bin/positor
 go install github.com/ahyangyi/gandalf/cmd@f44011df
 mv gopath/bin/cmd gopath/bin/layer-filter
