51aa4af7dae4de450ee5c082a138d4d4d4f1dce8
Yi Yang @ Anteros <ahyangyi@gmail.com>
2024-04-21 22:51:02 +0800

    ..

---
+++ station/stations/dovemere_2018_lib/flexible_stations/common.py
@@ -0,0 +1,15 @@
+def horizontal_layout(l, r, onetile, twotile, lwall, general, window, window_extender, threetile=None):
+    threetile = threetile or twotile
+    if l + r == 0:
+        return onetile
+    if l + r == 1:
+        return [twotile, twotile.R][l]
+    if l + r == 2:
+        return [threetile, window_extender, threetile.R][l]
+
+    e = l + r - 3
+    c = (e + 1) // 3
+    if c % 2 != e % 2:
+        c += 1
+    o = (e - c) // 2
+    return ([lwall] + [general] * o + [window] + [window_extender] * c + [window.R] + [general] * o + [lwall.R])[l]
--- station/stations/dovemere_2018_lib/flexible_stations/semitraversable.py
+++ station/stations/dovemere_2018_lib/flexible_stations/semitraversable.py
@@ -2,6 +2,7 @@ import grf
 from station.lib import AStation, ALayout, AGroundSprite, AParentSprite, LayoutSprite, Demo
 from agrf.magic import Switch
 from ..layouts import named_tiles, layouts
+from .common import horizontal_layout
 
 named_tiles.globalize()
 
@@ -97,23 +98,6 @@ def get_left_index(t, d):
         return a[min(t - 1, (t - 1) % 2 + 2)]
     else:
         return a[min(d - 1, (d - 1) % 2 + 2)].T
-
-
-def horizontal_layout(l, r, onetile, twotile, lwall, general, window, window_extender, threetile=None):
-    threetile = threetile or twotile
-    if l + r == 0:
-        return onetile
-    if l + r == 1:
-        return [twotile, twotile.R][l]
-    if l + r == 2:
-        return [threetile, window_extender, threetile.R][l]
-
-    e = l + r - 3
-    c = (e + 1) // 3
-    if c % 2 != e % 2:
-        c += 1
-    o = (e - c) // 2
-    return ([lwall] + [general] * o + [window] + [window_extender] * c + [window.R] + [general] * o + [lwall.R])[l]
 
 
 left_wall = Switch(
