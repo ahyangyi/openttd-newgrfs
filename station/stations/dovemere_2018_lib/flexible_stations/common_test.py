3f431e1f4e06f309ce39942931e476945a0989fb
Yi Yang @ Aquamarine <ahyangyi@gmail.com>
2024-04-23 20:24:34 +0800

    (station) add more test cases

--- station/stations/dovemere_2018_lib/flexible_stations/common_test.py
+++ station/stations/dovemere_2018_lib/flexible_stations/common_test.py
@@ -1,9 +1,19 @@
 from station.stations.dovemere_2018_lib.flexible_stations.common import determine_platform_odd, determine_platform_even
 
 
+def check_platform_function(fn, n, expected):
+    assert expected == "".join(fn(t, n - 1 - t) for t in range(1, n - 1))
+
+
 def test_determine_platform_odd():
-    assert "".join(determine_platform_odd(t, 8 - t) for t in range(1, 8)) == "nfdddnf"
+    check_platform_function(determine_platform_odd, 7, "nfdnf")
+    check_platform_function(determine_platform_odd, 8, "nfnfnf")
+    check_platform_function(determine_platform_odd, 9, "nfdddnf")
+    check_platform_function(determine_platform_odd, 10, "nfnfnfnf")
 
 
 def test_determine_platform_even():
-    assert "".join(determine_platform_even(t, 8 - t) for t in range(1, 8)) == "fnfdnfn"
+    check_platform_function(determine_platform_even, 7, "fdddn")
+    check_platform_function(determine_platform_even, 8, "fnfnfn")
+    check_platform_function(determine_platform_even, 9, "fnfdnfn")
+    check_platform_function(determine_platform_even, 10, "fnfnfnfn")
