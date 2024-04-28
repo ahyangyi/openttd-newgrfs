a236c60104de7ee8e42ab0246bc19c0736a70acd
Yi Yang @ Anteros <ahyangyi@gmail.com>
2024-04-29 04:31:19 +0800

    (station) set version & mincompatversion

--- station/dovemere_gen.py
+++ station/dovemere_gen.py
@@ -23,6 +23,8 @@ def gen():
         grfid=b"\xE5\xBC\x8Bs",
         name=s["STR_GRF_NAME"],
         description=s["STR_GRF_DESC"],
+        version=1,
+        min_compatible_version=1,
         id_map_file="station/id_map.json",
         sprite_cache_path="station/.cache",
         strings=s,
