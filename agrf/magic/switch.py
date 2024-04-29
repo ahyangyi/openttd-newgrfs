1c09b6bcf606718feada3eb7f2f135ec63584dd6
Yi Yang @ Aquamarine <ahyangyi@gmail.com>
2024-04-29 17:57:52 +0800

    ...

--- agrf/magic/switch.py
+++ agrf/magic/switch.py
@@ -1,53 +1,23 @@
 import grf
-from collections.abc import Collection, Mapping, Hashable
+from .functor import CachedFunctorMixin
 
 
-def deep_freeze(thing):
-    if thing is None or isinstance(thing, str):
-        return thing
-    elif isinstance(thing, Mapping):
-        return tuple((k, deep_freeze(v)) for k, v in sorted(thing.items()))
-    elif isinstance(thing, Collection):
-        return tuple(deep_freeze(i) for i in thing)
-    elif not isinstance(thing, Hashable):
-        raise TypeError(f"unfreezable type: '{type(thing)}'")
-    else:
-        return thing
-
-
-class Switch(grf.Switch):
+class Switch(CachedFunctorMixin, grf.Switch):
     def __init__(self, code, ranges, default, *, feature=None, ref_id=None, related_scope=False, subroutines=None):
         super().__init__(
             code, ranges, default, feature=feature, ref_id=ref_id, related_scope=related_scope, subroutines=subroutines
         )
-        self.attr_cache = {}
-        self.call_cache = {}
 
-    def derive(self, callback):
+    def fmap(self, f):
         return Switch(
             self.code,
-            {(r.low, r.high): callback(r.ref) for r in self._ranges},
-            callback(self.default),
+            {(r.low, r.high): f(r.ref) for r in self._ranges},
+            f(self.default),
             feature=self.feature,
             ref_id=self.ref_id,
             related_scope=self.related_scope,
             subroutines=self.subroutines,
         )
-
-    def __getattr__(self, name):
-        if name in self.attr_cache:
-            return self.attr_cache[name]
-        ret = self.derive(lambda x: getattr(x, name))
-        self.attr_cache[name] = ret
-        return ret
-
-    def __call__(self, *args, **kwargs):
-        key = deep_freeze((args, kwargs))
-        if key in self.call_cache:
-            return self.call_cache[key]
-        ret = self.derive(lambda x: x(*args, **kwargs))
-        self.call_cache[key] = ret
-        return ret
 
     # FIXME: should probably be refactored into something more general
     # and not rely on everything else implementing `get_default_graphics`
--- station/lib/switches.py
+++ station/lib/switches.py
@@ -2,6 +2,11 @@ from agrf.magic.switch import Switch
 
 
 class StationTileSwitch(Switch):
+    def __init__(self, var, ranges, default):
+        code = StationTileSwitch.var2code[var]
+        super().__init__(code, ranges, default)
+        self.var = var
+
     var2code = {
             "t": "var(0x41, shift=12, and=0x0000000f)"
             "d": "var(0x41, shift=8, and=0x0000000f)"
@@ -9,6 +14,17 @@ class StationTileSwitch(Switch):
             "r": "var(0x41, shift=0, and=0x0000000f)"
         }
 
+    def derive(self, callback):
+        return StationTileSwitch(
+            self.var,
+            {(r.low, r.high): callback(r.ref) for r in self._ranges},
+            callback(self.default),
+        )
+
     def lookup(self, w, h, x, y):
+        if self.var == "l":
+            pass
+        else:
+            raise NotImplementedError()
 
 def make_horizontal_switch(cb):
