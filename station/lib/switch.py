4116e5a985f43a05d13dd76ef15684db75357238
Yi Yang @ Anteros <ahyangyi@gmail.com>
2024-04-30 04:40:49 +0800

    ..

--- station/lib/switch.py
+++ station/lib/switch.py
@@ -2,30 +2,32 @@ from agrf.magic import Switch
 from .layout import ALayout
 
 
-def lookup(thing, w, h, x, y):
-    if isinstance(thing, ALayout):
+def lookup(thing, w, h, x, y, t):
+    if isinstance(thing, (ALayout, int)):
         return thing
-    return thing.lookup(w, h, x, y)
+    return thing.lookup(w, h, x, y, t)
 
 
 class StationTileSwitch:
-    def __init__(self, var, ranges):
-        code = StationTileSwitch.var2code[var]
+    def __init__(self, var, ranges, cb24=False):
         self.var = var
         self.ranges = ranges
+        self.cb24 = cb24
 
-    var2code = {
-        "t": "var(0x41, shift=12, and=0x0000000f)",
-        "d": "var(0x41, shift=8, and=0x0000000f)",
-        "l": "var(0x41, shift=4, and=0x0000000f)",
-        "r": "var(0x41, shift=0, and=0x0000000f)",
-    }
+    @property
+    def code(self):
+        nibble = {"T": 24, "t": 12, "d": 8, "l": 4, "r": 0}[self.var]
+
+        if self.cb24:
+            return (f"(extra_callback_info1 >> {nibble}) & 0xf",)
+        else:
+            return (f"var(0x41, shift={nibble}, and=0x0000000f)",)
 
     def fmap(self, f, special_property=None):
         new_var = (
             {"T": {"t": "d", "d": "t"}, "R": {"l": "r", "r": "l"}}.get(special_property, {}).get(self.var, self.var)
         )
-        return StationTileSwitch(new_var, {k: f(v) for k, v in self.ranges.items()})
+        return StationTileSwitch(new_var, {k: f(v) for k, v in self.ranges.items()}, cb24=self.cb24)
 
     @property
     def T(self):
@@ -37,17 +39,19 @@ class StationTileSwitch:
 
     def to_index(self, sprite_list):
         new_ranges = {k: v.to_index(sprite_list) for k, v in self.ranges.items()}
-        return Switch(ranges=new_ranges, default=min(new_ranges.items())[1], code=self.var2code[self.var])
+        return Switch(ranges=new_ranges, default=min(new_ranges.items())[1], code=self.code)
 
-    def lookup(self, w, h, x, y):
-        if self.var == "l":
-            return lookup(self.ranges[min(x, 15)], w, h, x, y)
+    def lookup(self, w, h, x, y, t=0):
+        if self.var == "T":
+            return lookup(self.ranges[t & 0x7], w, h, x, y, t)
+        elif self.var == "l":
+            return lookup(self.ranges[min(x, 15)], w, h, x, y, t)
         elif self.var == "r":
-            return lookup(self.ranges[min(w - x - 1, 15)], w, h, x, y)
+            return lookup(self.ranges[min(w - x - 1, 15)], w, h, x, y, t)
         elif self.var == "t":
-            return lookup(self.ranges[min(y, 15)], w, h, x, y)
+            return lookup(self.ranges[min(y, 15)], w, h, x, y, t)
         elif self.var == "d":
-            return lookup(self.ranges[min(h - y - 1, 15)], w, h, x, y)
+            return lookup(self.ranges[min(h - y - 1, 15)], w, h, x, y, t)
         else:
             raise NotImplementedError()
 
@@ -61,3 +65,7 @@ def make_horizontal_switch(f):
 
 def make_vertical_switch(f):
     return StationTileSwitch("t", {t: StationTileSwitch("d", {d: f(t, d) for d in range(16)}) for t in range(16)})
+
+
+def two_cb_demo(cb24, cb14):
+    return [[cb14.lookup(w, h, x, y, cb24.lookup(w, h, x, y)) for x in range(w)] for y in range(h)]
