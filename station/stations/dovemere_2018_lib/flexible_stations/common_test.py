b6df94d33024e3a3d0f7282fcbf88ce41c3d949c
Yi Yang @ Anteros <ahyangyi@gmail.com>
2024-05-01 01:23:43 +0800

    ..

--- station/stations/dovemere_2018_lib/flexible_stations/common_test.py
+++ station/stations/dovemere_2018_lib/flexible_stations/common_test.py
@@ -2,18 +2,18 @@ from station.stations.dovemere_2018_lib.flexible_stations.common import determin
 
 
 def check_platform_function(fn, n, expected):
-    assert expected == "".join(fn(t, n - 1 - t) for t in range(1, n - 1))
+    assert expected == "".join(fn(t, n - 1 - t) for t in range(n))
 
 
 def test_determine_platform_odd():
-    check_platform_function(determine_platform_odd, 7, "fndfn")
-    check_platform_function(determine_platform_odd, 8, "fnfnfn")
-    check_platform_function(determine_platform_odd, 9, "fndddfn")
-    check_platform_function(determine_platform_odd, 10, "fnfnfnfn")
+    check_platform_function(determine_platform_odd, 7, "nfndfnf")
+    check_platform_function(determine_platform_odd, 8, "nfnfnfnf")
+    check_platform_function(determine_platform_odd, 9, "nfndddfnf")
+    check_platform_function(determine_platform_odd, 10, "nfnfnfnfnf")
 
 
 def test_determine_platform_even():
-    check_platform_function(determine_platform_even, 7, "ndddf")
-    check_platform_function(determine_platform_even, 8, "nfnfnf")
-    check_platform_function(determine_platform_even, 9, "nfndfnf")
-    check_platform_function(determine_platform_even, 10, "nfnfnfnf")
+    check_platform_function(determine_platform_even, 7, "fndddfn")
+    check_platform_function(determine_platform_even, 8, "fnfnfnfn")
+    check_platform_function(determine_platform_even, 9, "fnfndfnfn")
+    check_platform_function(determine_platform_even, 10, "fnfnfnfnfn")
