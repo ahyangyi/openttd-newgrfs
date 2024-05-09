ff232223e46097e4ce1c6148d32a18071b113bfc
Yi Yang @ Anteros <ahyangyi@gmail.com>
2024-05-09 12:23:59 +0800

    ..

--- station/stations/dovemere_2018_lib/demos/semi_auto.py
+++ station/stations/dovemere_2018_lib/demos/semi_auto.py
@@ -1,26 +1,28 @@
 from station.lib import Demo
-from station.stations.dovemere_2018_lib.layouts import platform, platform_s, platform_s_nt, concourse_tile
+from station.stations.dovemere_2018_lib.layouts import platform, platform_s, concourse_tile
+from station.stations.platforms import named_tiles as platform_tiles
 from station.lib.utils import get_1cc_remap
 from agrf.graphics.palette import CompanyColour
 from station.stations.dovemere_2018_lib.flexible_stations import semitraversable
 from .utils import h_merge
 
+nt = platform_tiles.side_concourse_shed
+
 row_3 = h_merge(
-    [semitraversable.cb14a.demo(i, 3, semitraversable.cb24_0) for i in range(1, 5)],
-    [[platform_s_nt], [platform], [platform_s_nt.T]],
+    [semitraversable.cb14a.demo(i, 3, semitraversable.cb24_0) for i in range(1, 5)], [[nt], [platform], [nt.T]]
 )
 v_sep = [[concourse_tile] * len(row_3[0])] * 2
 row_4 = h_merge(
     [semitraversable.cb14a.demo(i, 4, semitraversable.cb24_0) for i in range(1, 5)],
-    [[platform_s_nt], [platform_s.T], [platform_s], [platform_s_nt.T]],
+    [[nt], [platform_s.T], [platform_s], [nt.T]],
 )
 row_5 = h_merge(
     [semitraversable.cb14a.demo(i, 5, semitraversable.cb24_0) for i in range(1, 5)],
-    [[platform_s_nt], [platform], [platform], [platform], [platform_s_nt.T]],
+    [[nt], [platform], [platform], [platform], [nt.T]],
 )
 row_6 = h_merge(
     [semitraversable.cb14a.demo(i, 6, semitraversable.cb24_0) for i in range(1, 5)],
-    [[platform_s_nt], [platform_s.T], [platform_s], [platform_s.T], [platform_s], [platform_s_nt.T]],
+    [[nt], [platform_s.T], [platform_s], [platform_s.T], [platform_s], [nt.T]],
 )
 
 semi_auto_demo = Demo(
