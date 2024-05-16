5f7835919b5dd7a2616cda4ab0e28d191b325ae9
Yi Yang @ Aquamarine <ahyangyi@gmail.com>
2024-05-16 19:27:15 +0800

    (station) remove unused imports

--- station/stations/dovemere_2018_lib/demos/full_np_auto.py
+++ station/stations/dovemere_2018_lib/demos/full_np_auto.py
@@ -1,5 +1,5 @@
 from station.lib import Demo
-from station.stations.dovemere_2018_lib.layouts import platform, platform_s, concourse_tile, track
+from station.stations.dovemere_2018_lib.layouts import platform, platform_s, concourse_tile
 from station.lib.utils import get_1cc_remap
 from agrf.graphics.palette import CompanyColour
 from station.stations.dovemere_2018_lib.flexible_stations import traversable
