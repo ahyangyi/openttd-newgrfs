14a327c44455e07bc95377c06405176eebf986c5
Yi Yang @ Anteros <ahyangyi@gmail.com>
2024-05-30 03:16:45 +0800

    ..

--- station/stations/dovemere_2018_lib/flexible_stations/semitraversable.py
+++ station/stations/dovemere_2018_lib/flexible_stations/semitraversable.py
@@ -8,24 +8,22 @@ from station.stations.platforms import platform_classes, shelter_classes
 
 named_tiles.globalize()
 
-front = make_front_row("_platform")
-front2 = make_front_row("")
 
 single = make_row(tiny_untraversable, h_end_gate_untraversable, h_end_untraversable, h_normal, h_gate, h_gate_extender)
-cb14_0a = make_vertical_switch(lambda t, d: single if d == t == 0 else front if d == 0 else front.T if t == 0 else None)
-cb14_0b = make_vertical_switch(
-    lambda t, d: single if d == t == 0 else front2 if d == 0 else front2.T if t == 0 else None
-)
 
 semitraversable_stations = []
 global_id = 0x00
 for pclass in platform_classes:
+    front = make_front_row("_platform")
     for sclass in shelter_classes:
         cb24 = make_vertical_switch(
             lambda t, d: 0 if t == 0 or d == 0 else {"n": 2, "f": 4, "d": 6}[determine_platform_odd(t, d)], cb24=True
         )
+        cb14_0 = make_vertical_switch(
+            lambda t, d: single if d == t == 0 else front if d == 0 else front.T if t == 0 else None
+        )
         cb14 = StationTileSwitch(
-            "T", {0: cb14_0a, 1: cb14_0a, 2: cb14_2, 3: cb14_2, 4: cb14_4, 5: cb14_4, 6: cb14_6, 7: cb14_6}
+            "T", {0: cb14_0, 1: cb14_0, 2: cb14_2, 3: cb14_2, 4: cb14_4, 5: cb14_4, 6: cb14_6, 7: cb14_6}
         )
         if pclass == "concrete" and sclass == "shelter_1":
             demo_1 = lambda r, c, cb14=cb14, cb24=cb24: cb14.demo(r, c, cb24)
@@ -48,13 +46,17 @@ for pclass in platform_classes:
         )
         global_id += 1
 
+front = make_front_row("")
 for pclass in platform_classes:
     for sclass in shelter_classes:
         cb24 = make_vertical_switch(
             lambda t, d: 0 if t == 0 or d == 0 else {"n": 2, "f": 4, "d": 6}[determine_platform_even(t, d)], cb24=True
         )
+        cb14_0 = make_vertical_switch(
+            lambda t, d: single if d == t == 0 else front if d == 0 else front.T if t == 0 else None
+        )
         cb14 = StationTileSwitch(
-            "T", {0: cb14_0b, 1: cb14_0b, 2: cb14_2, 3: cb14_2, 4: cb14_4, 5: cb14_4, 6: cb14_6, 7: cb14_6}
+            "T", {0: cb14_0, 1: cb14_0, 2: cb14_2, 3: cb14_2, 4: cb14_4, 5: cb14_4, 6: cb14_6, 7: cb14_6}
         )
         if pclass == "concrete" and sclass == "shelter_1":
             demo_2 = lambda r, c, cb14=cb14, cb24=cb24: cb14.demo(r, c, cb24)
