1a971297d7094c5c89f72754445d6a492c59eb56
Yi Yang <ahyangyi@gmail.com>
2024-06-02 00:21:54 +0800

    (station) v0.2.1: fix monorail & maglev graphics (#28)

--- station/stations/misc.py
+++ station/stations/misc.py
@@ -1,5 +1,6 @@
 import grf
 from station.lib import ADefaultGroundSprite, ALayout
+from station.lib.registers import Registers
 
-track_ground = ADefaultGroundSprite(1012, {"add": grf.Temp(0)})
+track_ground = ADefaultGroundSprite(1012, {"add": Registers.CLIMATE_RAIL_OFFSET})
 track = ALayout([track_ground], [], True)
