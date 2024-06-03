365a492cbe567b63beed574965a407b60317177b
Yi Yang @ Aquamarine <ahyangyi@gmail.com>
2024-06-03 18:05:52 +0800

    ..........

--- agrf/graphics/misc.py
+++ agrf/graphics/misc.py
@@ -1,3 +1,3 @@
 import grf
 
-SCALE_TO_ZOOM = {4: grf.ZOOM_4X, 2: grf.ZOOM_2X, 1: grf.ZOOM_NORMAL}
+SCALE_TO_ZOOM = {4: grf.ZOOM_4X, 2: grf.ZOOM_2X, 1: grf.ZOOM_NORMAL, 0.5: grf.ZOOM_OUT_2X, 0.25: grf.ZOOM_OUT_4X, 0.125: grf.ZOOM_OUT_8X}
