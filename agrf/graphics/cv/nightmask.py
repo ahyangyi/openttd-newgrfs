767ff89f94a8e6a7d653eebaa5bc8860ce3f98fa
Yi Yang @ Aquamarine <ahyangyi@gmail.com>
2024-06-05 17:47:14 +0800

    ........

--- agrf/graphics/cv/nightmask.py
+++ agrf/graphics/cv/nightmask.py
@@ -1,5 +1,16 @@
+import numpy as np
 from .. import LayeredImage
 
 
-def make_night_mask(img: LayeredImage) -> LayeredImage:
-    return img  # lol
+def make_night_mask(img: LayeredImage, darkness=0.75) -> LayeredImage:
+    assert img.alpha is not None
+
+    return LayeredImage(
+        xofs=img.xofs,
+        yofs=img.yofs,
+        w=img.w,
+        h=img.h,
+        rgb=np.zeros((img.h, img.w, 3), dtype=np.uint8),
+        alpha=(img.alpha * darkess).astype(np.uint8),
+        mask=None,
+    )
