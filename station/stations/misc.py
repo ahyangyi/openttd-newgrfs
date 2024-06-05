ae1aeb8ebb3123fa27cbe0d6be6a2e3976f303da
Yi Yang @ Anteros <ahyangyi@gmail.com>
2024-06-06 01:42:02 +0800

    (station) Add default ground and update demos accordingly

--- station/stations/misc.py
+++ station/stations/misc.py
@@ -3,6 +3,8 @@ from station.lib import ADefaultGroundSprite, AGroundSprite, ALayout
 from station.lib.registers import Registers
 
 track_ground = ADefaultGroundSprite(1012, flags={"add": Registers.CLIMATE_RAIL_OFFSET})
+default_ground = ADefaultGroundSprite(3981, flags={"add": Registers.CLIMATE_OFFSET})
 empty_ground = AGroundSprite(grf.EMPTY_SPRITE, flags={"add": Registers.CLIMATE_RAIL_OFFSET})
 
 track = ALayout([track_ground], [], True)
+default = ALayout([default_ground], [], False)
