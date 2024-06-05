0fa328d334501502842e5ca885552721191d2cdc
Yi Yang @ Anteros <ahyangyi@gmail.com>
2024-06-06 04:32:05 +0800

    ...

--- station/lib/parameters.py
+++ station/lib/parameters.py
@@ -4,6 +4,6 @@ from agrf.parameters import Parameter, ParameterList
 parameter_list = ParameterList(
     [
         Parameter("INTRODUCTION_YEAR", 0, {0: "DISABLED", 1: "ENABLED"}),
-        Parameter("E88A9CA_INTRODUCTION_YEAR", 0, limits=(0, 9999)),
+        Parameter("E88A9CA_INTRODUCTION_YEAR", 2005, limits=(0, 9999)),
     ]
 )
