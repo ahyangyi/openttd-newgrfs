baf9bbe0a80745ca05aa10392911835b5daaeaa7
Yi Yang @ Aquamarine <ahyangyi@gmail.com>
2024-06-07 18:35:19 +0800

    ..

--- station/lib/parameters.py
+++ station/lib/parameters.py
@@ -4,6 +4,7 @@ from agrf.parameters import Parameter, ParameterList
 parameter_list = ParameterList(
     [
         Parameter("INTRODUCTION_YEAR", 0, {0: "DISABLED", 1: "ENABLED"}),
+        Parameter("SNOW_TRACK", 0, {0: "ENABLED", 1: "DISABLED"}),
         Parameter("E88A9CA_INTRODUCTION_YEAR", 2005, limits=(0, 9999)),
     ]
 )
