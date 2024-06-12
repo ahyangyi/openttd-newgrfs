7113810ff39fc687f84e7e73263610a299221f63
Yi Yang @ Aquamarine <ahyangyi@gmail.com>
2024-06-12 18:39:07 +0800

    (station) remove confusing parentheses

--- station/stations/dovemere_2018_lib/common.py
+++ station/stations/dovemere_2018_lib/common.py
@@ -7,6 +7,6 @@ common_cb = {
     "availability": Switch(
         ranges={0: 0},
         default=1,
-        code=f"current_year > (var(0x7F, param={enabled}, shift=0, and=0xffffffff)) * (var(0x7F, param={station_param}, shift=0, and=0xffffffff))",
+        code=f"current_year > (var(0x7F, param={enabled}, shift=0, and=0xffffffff) * var(0x7F, param={station_param}, shift=0, and=0xffffffff))",
     )
 }
