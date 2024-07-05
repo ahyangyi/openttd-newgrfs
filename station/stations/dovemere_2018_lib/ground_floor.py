ef1f272bdabf359cf453fc2f6a1609f11ffe49b7
Yi Yang @ Aquamarine <ahyangyi@gmail.com>
2024-07-05 18:41:41 +0800

    ..

--- station/stations/dovemere_2018_lib/ground_floor.py
+++ station/stations/dovemere_2018_lib/ground_floor.py
@@ -9,6 +9,15 @@ class GroundFloorElement:
     breaks_y_symmetry: bool
 
 
+ground_floor_elements = [
+    GroundFloorElement("full", ["ground level"], False),
+    GroundFloorElement("platform", ["ground level - platform"], True),
+    GroundFloorElement("third", ["ground level - third"], True),
+    GroundFloorElement("third_t", ["ground level - third - t"], True),
+]
+ground_floor_elements_lookup = {x.name: x for x in ground_floor_elements}
+
+
 @dataclass
 class GroundFloorStyle:
     name: str
