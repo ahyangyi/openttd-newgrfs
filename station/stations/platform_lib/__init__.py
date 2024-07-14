ahyangyi/openttd-newgrfs

commit e8ffa4820ccd1d1b261b75c315b85e68f4580dbf

(station) refactor platform sprites management (#38)

station/stations/platform_lib/__init__.py
@@ -1,0 +1,131 @@
+from station.lib import AttrDict, ALayout, BuildingSpriteSheetSymmetricalX, BuildingSpriteSheetSymmetrical
+from abc import ABC, abstractmethod
+from ..misc import track_ground
+from ..ground import named_ps as ground_ps
+
+gray_ps = ground_ps.gray
+
+named_ps = AttrDict()
+named_tiles = AttrDict()
+entries = []
+
+
+class PlatformFamily(ABC):
+    @abstractmethod
+    def get_platform_classes(self):
+        pass
+
+    @abstractmethod
+    def get_shelter_classes(self):
+        pass
+
+    @abstractmethod
+    def get_sprite(self, location, rail_facing, platform_class, shelter_class):
+        pass
+
+    @abstractmethod
+    def get_concourse_sprite(self, location, rail_facing, platform_class, shelter_class):
+        pass
+
+
+def us(s: str):
+    # FIXME
+    if s == "concrete":
+        return ""
+    return "_" + s if s != "" else s
+
+
+def register(pf: PlatformFamily):
+    platform_classes = pf.get_platform_classes()
+    shelter_classes = pf.get_shelter_classes()
+    name = "cns"  # FIXME
+
+    for platform_class in ["np", "cut"] + platform_classes:
+        for shelter_class in ["", "pillar"] + shelter_classes:
+            if (platform_class, shelter_class) == ("np", ""):
+                # Don't create the "nothing" tile
+                continue
+
+            if platform_class in ["np", "cut"]:
+                rail_facings = [""]
+            else:
+                rail_facings = ["", "side"]
+
+            if shelter_class == "":
+                locations = [""]
+            elif shelter_class == "pillar":
+                locations = ["", "building", "central"]
+            else:
+                locations = ["", "building", "building_v"]
+
+            for location in locations:
+                for rail_facing in rail_facings:
+                    suffix = f"{us(platform_class)}{us(rail_facing)}{us(shelter_class)}{us(location)}"
+                    ps = pf.get_sprite(location, rail_facing, platform_class, shelter_class)
+                    named_ps[name + suffix] = ps
+
+                    for l, make_symmetrical, extra_suffix in [([ps], False, ""), ([ps, ps.T], True, "_d")]:
+                        if make_symmetrical:
+                            cur_symmetry = ps.sprite.symmetry.add_y_symmetry()
+                        else:
+                            cur_symmetry = ps.sprite.symmetry
+                        var = cur_symmetry.get_all_variants(ALayout([track_ground], l, True))
+                        l = cur_symmetry.create_variants(var)
+                        if platform_class not in ["np", "cut"] and shelter_class != "pillar" and location == "":
+                            entries.extend(cur_symmetry.get_all_entries(l))
+                        named_tiles[name + suffix + extra_suffix] = l
+
+    for platform_class in platform_classes:
+        for rail_facing_1 in ["", "side"]:
+            for rail_facing_2 in ["", "side"]:
+                for shelter_class in [""] + shelter_classes:
+                    for shelter_class_2 in [""] + shelter_classes:
+                        if (shelter_class == "" or shelter_class_2 == "" or shelter_class == shelter_class_2) and (
+                            (rail_facing_1, shelter_class) < (rail_facing_2, shelter_class_2)
+                        ):
+                            suffix = f"{us(platform_class)}{us(rail_facing)}{us(shelter_class)}"
+                            suffix2 = f"{us(platform_class)}{us(rail_facing_2)}{us(shelter_class_2)}"
+                            cur_symmetry = BuildingSpriteSheetSymmetricalX
+                            var = cur_symmetry.get_all_variants(
+                                ALayout([track_ground], [named_ps[name + suffix], named_ps[name + suffix2].T], True)
+                            )
+                            l = cur_symmetry.create_variants(var)
+                            entries.extend(cur_symmetry.get_all_entries(l))
+                            named_tiles[name + suffix + "_and" + suffix2] = l
+                            named_tiles[name + suffix2 + "_and" + suffix] = l.T
+
+    name = "concourse"  # FIXME
+    for platform_class in [""] + platform_classes:
+        for side in ["", "d"] if platform_class != "" else [""]:
+            concourse_flavor = us(platform_class) + ("_side" if platform_class != "" else "") + us(side)
+            ps = pf.get_concourse_sprite(platform_class, side)
+            named_ps[name + concourse_flavor] = ps
+
+            if platform_class == "" or side == "d":
+                symmetry = BuildingSpriteSheetSymmetrical
+            else:
+                symmetry = BuildingSpriteSheetSymmetricalX
+
+            var = symmetry.get_all_variants(ALayout([gray_ps], [ps], False, notes={"concourse"}))
+            l = symmetry.create_variants(var)
+            entries.extend(symmetry.get_all_entries(l))
+            named_tiles[name + concourse_flavor] = l
+
+            if platform_class != "":
+                for shelter_class in shelter_classes:
+                    shelter = named_ps["cns_cut_" + shelter_class]
+                    for l, needs_symmetrical, extra_suffix in [
+                        ([shelter], False, ""),
+                        ([shelter, shelter.T], True, "_d"),
+                    ]:
+                        if needs_symmetrical:
+                            if side == "d":
+                                cur_sym = symmetry
+                            else:
+                                continue
+                        else:
+                            cur_sym = BuildingSpriteSheetSymmetricalX
+                        var = cur_sym.get_all_variants(ALayout([gray_ps], l + [ps], False, notes={"concourse"}))
+                        l = cur_sym.create_variants(var)
+                        entries.extend(cur_sym.get_all_entries(l))
+                        named_tiles[name + concourse_flavor + us(shelter_class) + extra_suffix] = l

station/lib/binary_variants.py
@@ -10,6 +10,7 @@ class BinaryVariantMixin:
                 v.M = variants[i ^ classobj._m_offset]
             v.R = variants[i ^ classobj._r_offset]
             v.T = variants[i ^ classobj._t_offset]
+            v.symmetry = classobj
         return variants[0]
 
     @classmethod

station/stations/platforms.py
@@ -13,6 +13,7 @@ from station.lib import (
 from agrf.graphics.voxel import LazyVoxel
 from .ground import named_ps as ground_ps
 from .misc import track_ground
+from station.stations.platform_lib import PlatformFamily, register, named_ps, named_tiles, entries
 
 
 gray_ps = ground_ps.gray
@@ -24,18 +25,80 @@ shelter_height = 17
 pillar_height = 18
 
 
+class CNSPlatformFamily(PlatformFamily):
+    def __init__(self):
+        self.v = LazyVoxel(
+            "cns",
+            prefix="station/voxels/render/cns",
+            voxel_getter=lambda path="station/voxels/cns/cns.vox": path,
+            load_from="station/files/cns-gorender.json",
+        )
+        self.concourse = LazyVoxel(
+            "concourse",
+            prefix="station/voxels/render/cns",
+            voxel_getter=lambda path="station/voxels/cns/concourse.vox": path,
+            load_from="station/files/cns-gorender.json",
+        )
+
+    def get_platform_classes(self):
+        return ["concrete", "brick"]
+
+    def get_shelter_classes(self):
+        return ["shelter_1", "shelter_2"]
+
+    def get_sprite(self, location, rail_facing, platform_class, shelter_class):
+        if platform_class == "":
+            pkeeps = set()
+        else:
+            pkeeps = {platform_class + ("_side" if rail_facing == "side" else "")}
+        if shelter_class == "":
+            skeeps = set()
+        else:
+            skeeps = {shelter_class + ("_" if location != "" else "") + location}
+
+        v2 = self.v.discard_layers(
+            tuple(sorted(tuple(platform_components - pkeeps) + tuple(shelter_components - skeeps))),
+            f"subset_{platform_class}_{rail_facing}_{shelter_class}_{location}",
+        )
+        if location == "building":
+            symmetry = BuildingSpriteSheetFull
+        else:
+            symmetry = BuildingSpriteSheetSymmetricalX
+        v2.in_place_subset(symmetry.render_indices())
+        foundation_height = platform_height if platform_class == "cut" else 0
+        sprite = symmetry.create_variants(
+            v2.spritesheet(xdiff=16 - platform_width, xspan=platform_width, zdiff=foundation_height * 2)
+        )
+
+        height = max((platform_height if platform_class != "" else 0), (shelter_height if shelter_class != "" else 0))
+        return AParentSprite(
+            sprite, (16, platform_width, height - foundation_height), (0, 16 - platform_width, foundation_height)
+        )
+
+    def get_concourse_sprite(self, platform_class, side):
+        if platform_class == "":
+            ckeeps = set()
+        elif side == "d":
+            ckeeps = {platform_class, platform_class + "_t"}
+        else:
+            ckeeps = {platform_class}
+
+        if platform_class == "" or side == "d":
+            symmetry = BuildingSpriteSheetSymmetrical
+        else:
+            symmetry = BuildingSpriteSheetSymmetricalX
+
+        v2 = self.concourse.discard_layers(
+            tuple(sorted(tuple(concourse_components - ckeeps))), f"subset_{platform_class}_{side}"
+        )
+        v2.in_place_subset(symmetry.render_indices())
+
+        sprite = symmetry.create_variants(v2.spritesheet())
+        return AParentSprite(sprite, (16, 16, platform_height), (0, 0, 0))
+
+
 platform_components = {"cut", "concrete", "concrete_side", "brick", "brick_side"}
 platform_classes = ["concrete", "brick"]
-platform_meta = [
-    ("_np", "", False, set(), 0),
-    ("_cut", "", False, {"cut"}, platform_height),
-    ("", "concrete", True, {"concrete"}, platform_height),
-    ("_side", "concrete", True, {"concrete_side"}, platform_height),
-    ("_brick", "brick", True, {"brick"}, platform_height),
-    ("_brick_side", "brick", True, {"brick_side"}, platform_height),
-]
-
-
 shelter_components = {
     "shelter_1",
     "shelter_1_building",
@@ -48,172 +111,11 @@ shelter_components = {
     "pillar_central",
 }
 shelter_classes = ["shelter_1", "shelter_2"]
-shelter_meta = [
-    ("", "", BuildingSpriteSheetSymmetricalX, set(), 0, True),
-    ("_shelter_1", "shelter_1", BuildingSpriteSheetSymmetricalX, {"shelter_1"}, shelter_height, True),
-    ("_shelter_1_building", "shelter_1", BuildingSpriteSheetFull, {"shelter_1_building"}, shelter_height, False),
-    (
-        "_shelter_1_building_v",
-        "shelter_1",
-        BuildingSpriteSheetSymmetricalX,
-        {"shelter_1_building_v"},
-        shelter_height,
-        False,
-    ),
-    ("_shelter_2", "shelter_2", BuildingSpriteSheetSymmetricalX, {"shelter_2"}, shelter_height, True),
-    ("_shelter_2_building", "shelter_2", BuildingSpriteSheetFull, {"shelter_2_building"}, shelter_height, False),
-    (
-        "_shelter_2_building_v",
-        "shelter_2",
-        BuildingSpriteSheetSymmetricalX,
-        {"shelter_2_building_v"},
-        shelter_height,
-        False,
-    ),
-    ("_pillar", "pillar", BuildingSpriteSheetSymmetricalX, {"pillar"}, pillar_height, False),
-    ("_pillar_building", "pillar", BuildingSpriteSheetFull, {"pillar_building"}, pillar_height, False),
-    ("_pillar_central", "pillar", BuildingSpriteSheetSymmetricalX, {"pillar_central"}, pillar_height, False),
-]
+concourse_components = {f"{c}{postfix}" for c in platform_classes for postfix in ["", "_t"]}
 
 
-def quickload(name):
-    v = LazyVoxel(
-        name,
-        prefix="station/voxels/render/cns",
-        voxel_getter=lambda path=f"station/voxels/cns/{name}.vox": path,
-        load_from="station/files/cns-gorender.json",
-    )
-
-    for platform_flavor, _pclass, pbuildable, pkeeps, pheight in platform_meta:
-        for shelter_flavor, _sclass, symmetry, skeeps, sheight, sbuildable in shelter_meta:
-            if (platform_flavor, shelter_flavor) == ("_np", ""):
-                # Don't create the "nothing" tile
-                continue
-
-            suffix = platform_flavor + shelter_flavor
-            v2 = v.discard_layers(
-                tuple(sorted(tuple(platform_components - pkeeps) + tuple(shelter_components - skeeps))),
-                "subset" + suffix,
-            )
-            v2.in_place_subset(symmetry.render_indices())
-            foundation_height = platform_height if platform_flavor == "_cut" else 0
-            sprite = symmetry.create_variants(
-                v2.spritesheet(xdiff=16 - platform_width, xspan=platform_width, zdiff=foundation_height * 2)
-            )
-
-            height = max(pheight, sheight)
-            ps = AParentSprite(
-                sprite, (16, platform_width, height - foundation_height), (0, 16 - platform_width, foundation_height)
-            )
-            named_ps[name + suffix] = ps
-
-            for l, make_symmetrical, extra_suffix in [([ps], False, ""), ([ps, ps.T], True, "_d")]:
-                if make_symmetrical:
-                    cur_symmetry = symmetry.add_y_symmetry()
-                else:
-                    cur_symmetry = symmetry
-                var = cur_symmetry.get_all_variants(ALayout([track_ground], l, True))
-                l = cur_symmetry.create_variants(var)
-                if sbuildable and pbuildable:
-                    entries.extend(cur_symmetry.get_all_entries(l))
-                named_tiles[name + suffix + extra_suffix] = l
-
-    for platform_flavor, pclass, pbuildable, _, _ in platform_meta:
-        if pbuildable:
-            for shelter_flavor, sclass, _, _, _, sbuildable in shelter_meta:
-                if sbuildable:
-                    for platform_flavor_2, pclass2, pbuildable_2, _, _ in platform_meta:
-                        if pbuildable_2 and (pclass == "" or pclass2 == "" or pclass == pclass2):
-                            for shelter_flavor_2, sclass2, _, _, _, sbuildable_2 in shelter_meta:
-                                if (
-                                    sbuildable_2
-                                    and (sclass == "" or sclass2 == "" or sclass == sclass2)
-                                    and ((platform_flavor, shelter_flavor) < (platform_flavor_2, shelter_flavor_2))
-                                ):
-                                    cur_symmetry = BuildingSpriteSheetSymmetricalX
-                                    var = cur_symmetry.get_all_variants(
-                                        ALayout(
-                                            [track_ground],
-                                            [
-                                                named_ps[name + platform_flavor + shelter_flavor],
-                                                named_ps[name + platform_flavor_2 + shelter_flavor_2].T,
-                                            ],
-                                            True,
-                                        )
-                                    )
-                                    l = cur_symmetry.create_variants(var)
-                                    entries.extend(cur_symmetry.get_all_entries(l))
-                                    named_tiles[
-                                        name
-                                        + platform_flavor
-                                        + shelter_flavor
-                                        + "_and"
-                                        + platform_flavor_2
-                                        + shelter_flavor_2
-                                    ] = l
-                                    named_tiles[
-                                        name
-                                        + platform_flavor_2
-                                        + shelter_flavor_2
-                                        + "_and"
-                                        + platform_flavor
-                                        + shelter_flavor
-                                    ] = l.T
-
-
-def simple_load(name):
-    v = LazyVoxel(
-        name,
-        prefix="station/voxels/render/cns",
-        voxel_getter=lambda path=f"station/voxels/cns/{name}.vox": path,
-        load_from="station/files/cns-gorender.json",
-    )
-    concourse_components = {f"{c}{postfix}" for c in platform_classes for postfix in ["", "_t"]}
-
-    for concourse_flavor, symmetry, ckeeps in [
-        ("", BuildingSpriteSheetSymmetrical, set()),
-        ("_side", BuildingSpriteSheetSymmetricalX, {"concrete"}),
-        ("_side_d", BuildingSpriteSheetSymmetrical, {"concrete", "concrete_t"}),
-        ("_brick_side", BuildingSpriteSheetSymmetricalX, {"brick"}),
-        ("_brick_side_d", BuildingSpriteSheetSymmetrical, {"brick", "brick_t"}),
-    ]:
-        v2 = v.discard_layers(tuple(sorted(tuple(concourse_components - ckeeps))), "subset" + concourse_flavor)
-        v2.in_place_subset(symmetry.render_indices())
-
-        sprite = symmetry.create_variants(v2.spritesheet())
-        ps = AParentSprite(sprite, (16, 16, platform_height), (0, 0, 0))
-        named_ps[name + concourse_flavor] = ps
-
-        var = symmetry.get_all_variants(ALayout([gray_ps], [ps], False, notes={"concourse"}))
-        l = symmetry.create_variants(var)
-        entries.extend(symmetry.get_all_entries(l))
-        named_tiles[name + concourse_flavor] = l
-
-        if concourse_flavor != "":
-            for shelter_flavor, _, _, _, _, buildable in shelter_meta:
-                if shelter_flavor == "" or not buildable:
-                    continue
-                shelter = named_ps["cns_cut" + shelter_flavor]
-                for l, needs_symmetrical, extra_suffix in [([shelter], False, ""), ([shelter, shelter.T], True, "_d")]:
-                    if needs_symmetrical:
-                        if concourse_flavor.endswith("_d"):
-                            cur_sym = symmetry
-                        else:
-                            continue
-                    else:
-                        cur_sym = BuildingSpriteSheetSymmetricalX
-                    var = cur_sym.get_all_variants(ALayout([gray_ps], l + [ps], False, notes={"concourse"}))
-                    l = cur_sym.create_variants(var)
-                    entries.extend(cur_sym.get_all_entries(l))
-                    named_tiles[name + concourse_flavor + shelter_flavor + extra_suffix] = l
-
-
-entries = []
-named_ps = AttrDict()
-named_tiles = AttrDict()
-
-quickload("cns")
-simple_load("concourse")
+pf = CNSPlatformFamily()
+register(pf)
 
 named_tiles.globalize()
 

commit fb0fafe5ec70883240ce79fa9e87048d43d88a70

(station) Bugfix: used wrong platforms, introduced by #38

station/stations/platform_lib/__init__.py
@@ -76,12 +76,12 @@ def register(pf: PlatformFamily):
                         named_tiles[name + suffix + extra_suffix] = l
 
     for platform_class in platform_classes:
-        for rail_facing_1 in ["", "side"]:
+        for rail_facing in ["", "side"]:
             for rail_facing_2 in ["", "side"]:
                 for shelter_class in [""] + shelter_classes:
                     for shelter_class_2 in [""] + shelter_classes:
                         if (shelter_class == "" or shelter_class_2 == "" or shelter_class == shelter_class_2) and (
-                            (rail_facing_1, shelter_class) < (rail_facing_2, shelter_class_2)
+                            (rail_facing, shelter_class) < (rail_facing_2, shelter_class_2)
                         ):
                             suffix = f"{us(platform_class)}{us(rail_facing)}{us(shelter_class)}"
                             suffix2 = f"{us(platform_class)}{us(rail_facing_2)}{us(shelter_class_2)}"