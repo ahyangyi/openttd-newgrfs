da8c5633edec061fdf0ddc67e0384d9dadf4c58a
Yi Yang <ahyangyi@gmail.com>
2024-08-19 21:37:46 +0800

    (stations) Add snow on roof (variant style) (#36)

--- station/stations/dovemere_2018_lib/layouts.py
+++ station/stations/dovemere_2018_lib/layouts.py
@@ -11,6 +11,7 @@ from station.lib import (
     AChildSprite,
     ALayout,
     AttrDict,
+    Registers,
 )
 from agrf.graphics.voxel import LazyVoxel
 from station.stations.platforms import (
@@ -27,7 +28,7 @@ from station.stations.platforms import (
 )
 from station.stations.ground import named_ps as ground_ps, named_tiles as ground_tiles, gray, gray_third
 from station.stations.misc import track_ground, track
-from agrf.graphics.recolour import PROCESS_COLOUR
+from agrf.graphics.recolour import NON_RENDERABLE_COLOUR
 from dataclasses import dataclass
 
 
@@ -106,8 +107,9 @@ V = make_hpos("", "_shelter_building_v")
 TinyAsym = make_hpos("_central", "_pillar_central")
 
 
+snow_layers = ("snow", "snow-window", "snow-window-extender")
 all_f2_layers = ("window", "window-extender")
-all_f2_layers_set = set(all_f2_layers)
+all_f2_layers_set = set(all_f2_layers + snow_layers)
 
 
 all_f1_layers = (
@@ -120,7 +122,7 @@ all_f1_layers = (
     "pillar",
     "pillar - t",
 )
-all_f1_layers_set = set(all_f1_layers)
+all_f1_layers_set = set(all_f1_layers + snow_layers)
 
 
 f1_subsets = {
@@ -132,24 +134,32 @@ f1_subsets = {
 
 
 def make_f2(v, sym):
-    v = v.discard_layers(all_f1_layers + all_f2_layers, "f2")
+    v = v.discard_layers(all_f1_layers + all_f2_layers + snow_layers, "f2")
     v.in_place_subset(sym.render_indices())
     v.config["agrf_manual_crop"] = (0, 10)
     s = sym.create_variants(v.spritesheet(zdiff=base_height * 2))
     return AParentSprite(s, (16, 16, overpass_height), (0, 0, base_height + platform_height))
 
 
-def make_f2_extra(v, sym, name):
-    v2 = v.discard_layers(all_f1_layers + all_f2_layers, "f2")
-    v = v.discard_layers(
-        all_f1_layers + tuple(all_f2_layers_set - {name}) + ("overpass", "foundation", "circle"), f"f2_{name}"
+def make_extra(v, sym, name, floor="f2"):
+    vd = v.discard_layers(
+        all_f1_layers + tuple(all_f2_layers_set - {name}) + ("overpass", "foundation", "circle"), name
     )
-    v = v.compose(v2, "merge", ignore_mask=True, colour_map=PROCESS_COLOUR)
+    if floor == "f2":
+        vd = vd.mask_clip_away("station/voxels/dovemere_2018/masks/ground_level.vox", "f2")
+    else:
+        vd = vd.mask_clip_away("station/voxels/dovemere_2018/masks/overpass.vox", "f1")
+    v = vd.compose(v, "merge", ignore_mask=True, colour_map=NON_RENDERABLE_COLOUR)
     v.config["agrf_palette"] = "station/files/ttd_palette_window.json"
+    if "snow" in name:
+        v.config["overlap"] = 1.3
     v.config["agrf_childsprite"] = (0, -10)
     v.in_place_subset(sym.render_indices())
     s = sym.create_variants(v.spritesheet())
-    return AChildSprite(s, (0, 0))
+    if "snow" in name:
+        return AChildSprite(s, (0, 0), flags={"dodraw": Registers.SNOW})
+    else:
+        return AChildSprite(s, (0, 0))
 
 
 f1_cache = {}
@@ -202,8 +212,11 @@ def load_central(source, symmetry, internal_category, name=None, h_pos=Normal, w
     name = name or source.split("/")[-1]
     v = make_voxel(source)
     f2 = make_f2(v, symmetry)
-    f2_window = make_f2_extra(v, symmetry.break_x_symmetry() if window_asym else symmetry, "window")
-    f2_window_extender = make_f2_extra(v, symmetry, "window-extender")
+    f2_window = make_extra(v, symmetry.break_x_symmetry() if window_asym else symmetry, "window")
+    f2_window_extender = make_extra(v, symmetry, "window-extender")
+    f2_snow = make_extra(v, symmetry, "snow")
+    f2_snow_window = make_extra(v, symmetry.break_x_symmetry() if window_asym else symmetry, "snow-window")
+    f2_snow_window_extender = make_extra(v, symmetry, "snow-window-extender")
 
     cur_np = h_pos.non_platform
     if window is None:
@@ -219,16 +232,17 @@ def load_central(source, symmetry, internal_category, name=None, h_pos=Normal, w
         else:
             f2_name = name + window_postfix
         if window_class == "none":
-            f2_component = [f2]
+            f2_component = [f2 + f2_snow]
             cur_sym = symmetry
         elif window_class == "windowed":
-            f2_component = [f2 + f2_window]
+            f2_component = [f2 + f2_window + f2_snow_window]
+            cur_sym = symmetry.break_x_symmetry()
             if window_asym:
                 cur_sym = symmetry.break_x_symmetry()
             else:
                 cur_sym = symmetry
         elif window_class == "windowed_extender":
-            f2_component = [f2 + f2_window_extender]
+            f2_component = [f2 + f2_window_extender + f2_snow_window_extender]
             cur_sym = symmetry.break_x_symmetry()
         register(
             ALayout(empty_ground, [cur_np, cur_np.T] + f2_component, True),
@@ -291,8 +305,24 @@ def load(
     name = name or source.split("/")[-1]
     v = make_voxel(source)
     f2 = make_f2(v, symmetry)
-    f2_window = make_f2_extra(v, symmetry.break_x_symmetry() if window_asym else symmetry, "window")
-    f2_window_extender = make_f2_extra(v, symmetry, "window-extender")
+    f2_window = make_extra(v, symmetry.break_x_symmetry() if window_asym else symmetry, "window")
+    f2_window_extender = make_extra(v, symmetry, "window-extender")
+    f2_snow = make_extra(v, symmetry, "snow")
+    f2_snow_window = make_extra(v, symmetry.break_x_symmetry() if window_asym else symmetry, "snow-window")
+    f2_snow_window_extender = make_extra(v, symmetry, "snow-window-extender")
+
+    if window is None:
+        window_classes = ["windowed"]
+    else:
+        window_classes = ["none"] + window
+
+    if "gate" in name or "tiny" in name:
+        if window is None:
+            f1_snow = make_extra(v, symmetry.break_x_symmetry() if window_asym else symmetry, "snow-window", floor="f1")
+        else:
+            f1_snow = make_extra(v, symmetry, "snow", floor="f1")
+    else:
+        f1_snow = None
 
     if borrow_f1 is not None:
         v = make_voxel(borrow_f1)
@@ -306,11 +336,6 @@ def load(
     plat_f1 = make_f1(v, "platform", broken_f1_symmetry)
     full_f1 = make_f1(v, "full", f1_symmetry)
 
-    if window is None:
-        window_classes = ["windowed"]
-    else:
-        window_classes = ["none"] + window
-
     for window_class in window_classes:
         window_postfix = "" if window_class == "none" else "_" + window_class
         if window is None:
@@ -320,11 +345,11 @@ def load(
         else:
             f2_name = name + window_postfix
         if window_class == "none":
-            f2_component = [f2]
+            f2_component = [f2 + f2_snow]
             cur_sym = symmetry
             cur_bsym = broken_symmetry
         elif window_class == "windowed":
-            f2_component = [f2 + f2_window]
+            f2_component = [f2 + f2_window + f2_snow_window]
             if window_asym:
                 cur_sym = symmetry.break_x_symmetry()
                 cur_bsym = broken_symmetry.break_x_symmetry()
@@ -332,7 +357,7 @@ def load(
                 cur_sym = symmetry
                 cur_bsym = broken_symmetry
         elif window_class == "windowed_extender":
-            f2_component = [f2 + f2_window_extender]
+            f2_component = [f2 + f2_window_extender + f2_snow_window_extender]
             cur_sym = symmetry
             cur_bsym = broken_symmetry
         for platform_class in platform_classes:
@@ -342,7 +367,9 @@ def load(
             pname = f2_name + platform_postfix
             if corridor:
                 register(
-                    ALayout(corridor_ground, [cur_plat, cur_plat.T, f1, f1b] + f2_component, True, notes=["third"]),
+                    ALayout(
+                        corridor_ground, [cur_plat, cur_plat.T, f1 + f1_snow, f1b] + f2_component, True, notes=["third"]
+                    ),
                     cur_sym,
                     internal_category,
                     pname + "_corridor",
@@ -350,7 +377,10 @@ def load(
             if third:
                 register(
                     ALayout(
-                        one_side_ground, [cur_plat, f1, h_pos.non_platform.T] + f2_component, True, notes=["third"]
+                        one_side_ground,
+                        [cur_plat, f1 + f1_snow, h_pos.non_platform.T] + f2_component,
+                        True,
+                        notes=["third"],
                     ),
                     cur_bsym,
                     internal_category,
@@ -363,7 +393,7 @@ def load(
                     register(
                         ALayout(
                             corridor_ground,
-                            [cur_plat_nt, f1, h_pos.platform(platform_class, shelter_class).T] + f2_component,
+                            [cur_plat_nt, f1 + f1_snow, h_pos.platform(platform_class, shelter_class).T] + f2_component,
                             True,
                             notes=["third", "far"],
                         ),
@@ -376,7 +406,7 @@ def load(
                         ALayout(
                             solid_ground,
                             [
-                                plat_f1,
+                                plat_f1 + f1_snow,
                                 h_pos.platform_back_cut(shelter_class).T,
                                 platform_ps[f"concourse{platform_postfix}_side"].T,
                             ]
@@ -390,7 +420,10 @@ def load(
                     )
         if full:
             register(
-                ALayout(solid_ground, [full_f1, concourse] + f2_component, False), cur_sym, internal_category, f2_name
+                ALayout(solid_ground, [full_f1 + f1_snow, concourse] + f2_component, False),
+                cur_sym,
+                internal_category,
+                f2_name,
             )
 
 
