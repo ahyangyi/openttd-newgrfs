e4df2ad74f50118de0497f5db6565b779ec759d3
Yi Yang @ Anteros <ahyangyi@gmail.com>
2024-08-29 21:03:36 +0800

    ..

--- station/stations/dovemere_2018_lib/objects.py
+++ station/stations/dovemere_2018_lib/objects.py
@@ -1,8 +1,13 @@
+import grf
 from grfobject.lib import AObject
 from station.lib import ALayout
 from .layouts import solid_ground
 
 test_layout = ALayout(solid_ground, [], True)
 test_object = AObject(
-    id=0x0, translation_name="STRAIGHT_STAIR", layouts=[test_layout, test_layout.T], class_label=b"\xe8\x8a\x9cZ"
+    id=0x0,
+    translation_name="STRAIGHT_STAIR",
+    layouts=[test_layout, test_layout.T],
+    class_label=b"\xe8\x8a\x9cZ",
+    climates_available=grf.ALL_CLIMATES,
 )
