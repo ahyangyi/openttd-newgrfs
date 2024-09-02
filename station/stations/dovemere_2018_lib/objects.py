e1266d57f54c8c78c8182c6380c03e102f3cabe6
Yi Yang @ Anteros <ahyangyi@gmail.com>
2024-09-02 14:36:03 +0800

    .............

--- station/stations/dovemere_2018_lib/objects.py
+++ station/stations/dovemere_2018_lib/objects.py
@@ -1,5 +1,5 @@
 import grf
-from station.lib import BuildingSpriteSheetFull, AGroundSprite, AParentSprite, ALayout
+from station.lib import BuildingSpriteSheetFull, BuildingSpriteSheetSymmetricalX, AGroundSprite, AParentSprite, ALayout
 from agrf.graphics.voxel import LazyVoxel
 from datetime import date
 from grfobject.lib import AObject
@@ -8,7 +8,7 @@ from .layouts import solid_ground
 
 objects = []
 
-for name in ["overpass", "west_stair"]:
+for name, sym in [("overpass", BuildingSpriteSheetSymmetricalX), ("west_stair", BuildingSpriteSheetFull)]:
     v = LazyVoxel(
         name,
         prefix="station/voxels/render/dovemere_2018/plaza",
@@ -16,20 +16,22 @@ for name in ["overpass", "west_stair"]:
         load_from="station/files/cns-gorender.json",
         # config={"z_scale": 1.01},
     )
-    sprite = BuildingSpriteSheetFull.create_variants(v.spritesheet())
+    sprite = sym.create_variants(v.spritesheet())
     ps = AParentSprite(sprite, (16, 16, 12), (0, 0, 0))
-    test_layout = ALayout(solid_ground, [ps], True)
-    test_object = AObject(
-        id=len(objects),
-        translation_name="STRAIGHT_STAIR",
-        layouts=[test_layout, test_layout.R, test_layout.T, test_layout.T.R],
-        class_label=b"\xe8\x8a\x9cZ",
-        climates_available=grf.ALL_CLIMATES,
-        size=(1, 1),
-        num_views=4,
-        introduction_date=0,
-        end_of_life_date=0,
-        height=1,
-        flags=grf.Object.Flags.ONLY_IN_GAME,
-    )
-    objects.append(test_object)
+    layout = ALayout(solid_ground, [ps], True)
+
+    for cur in [layout, layout.R] if (sym is BuildingSpriteSheetFull) else [layout]:
+        cur_object = AObject(
+            id=len(objects),
+            translation_name="STRAIGHT_STAIR",
+            layouts=[cur, cur.R.M, cur.T.R, cur.T.M],
+            class_label=b"\xe8\x8a\x9cZ",
+            climates_available=grf.ALL_CLIMATES,
+            size=(1, 1),
+            num_views=4,
+            introduction_date=0,
+            end_of_life_date=0,
+            height=1,
+            flags=grf.Object.Flags.ONLY_IN_GAME,
+        )
+        objects.append(cur_object)
