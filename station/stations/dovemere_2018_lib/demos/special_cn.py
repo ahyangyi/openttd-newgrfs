2f904fbd4582bbb042094d75d5413fca4f541038
Yi Yang @ Anteros <ahyangyi@gmail.com>
2024-10-04 01:23:35 +0800

    (station) Change demo `special_cn`; make it less crowded

--- station/stations/dovemere_2018_lib/demos/special_cn.py
+++ station/stations/dovemere_2018_lib/demos/special_cn.py
@@ -6,10 +6,18 @@ from agrf.graphics.palette import CompanyColour
 globalize_all(platform_class="concrete", shelter_class="shelter_1")
 
 top_half = [
-    [corner.T, front_gate.T, front_gate.T.R, funnel.T.R, h_normal_corridor, h_normal_corridor, h_end_corridor.R],
-    [side_a2_d, central_windowed_d, central_windowed_d.R, side_a3_d.T.R, cns_d, cns_d, cns_d],
-    [corner, inner_corner, central_d, side_a3_d.R, cns_d, cns_d, cns_d],
-    [concourse, corner, front_normal, double_corner.R, front_normal.T, corner.T.R, concourse],
+    [
+        corner_third.T,
+        front_gate_third.T,
+        front_gate_third.T.R,
+        funnel.T.R,
+        h_normal_corridor,
+        h_normal_corridor,
+        h_end_corridor.R,
+    ],
+    [side_a2_d, central_windowed_d, central_windowed_d.R, side_a3_d.T.R, cns, cns, cns],
+    [corner, inner_corner, central_f, side_a3_f.R, cns.T, cns.T, cns.T],
+    [concourse_none, corner, front_normal, double_corner.R, front_normal.T, corner.T.R, concourse],
 ]
 
 special_demo_cn = Demo(
