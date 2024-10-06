31d2005359f900d438a89f3adbdc507b19e9d709
Yi Yang @ Anteros <ahyangyi@gmail.com>
2024-10-06 23:23:37 +0800

    ...........

--- station/stations/dovemere_2018_lib/objects.py
+++ station/stations/dovemere_2018_lib/objects.py
@@ -14,7 +14,7 @@ from agrf.graphics.voxel import LazyVoxel
 from grfobject.lib import AObject
 
 named_grounds = AttrDict(schema=("name"))
-named_layouts = AttrDict(schema=("name"))
+named_layouts = AttrDict(schema=("name", "offset"))
 objects = []
 
 for name, sym in [("west_plaza_center", BuildingSymmetrical), ("west_plaza_offcenter", BuildingFull)]:
@@ -30,43 +30,7 @@ for name, sym in [("west_plaza_center", BuildingSymmetrical), ("west_plaza_offce
     named_grounds[(name,)] = gs
 
 
-for name, sym, xspan, yspan, height, osym in [
-    ("west_plaza_center", BuildingSymmetrical, 0, 0, 0, BuildingSymmetrical),
-    ("west_plaza_offcenter", BuildingFull, 0, 0, 0, BuildingFull),
-    ("west_plaza_center_flower_2021", BuildingSymmetrical, 4, 8, 6, BuildingSymmetrical),
-    ("west_plaza_center_flower_2022", BuildingSymmetrical, 4, 8, 6, BuildingSymmetrical),
-    ("west_plaza_center_flower_2023", BuildingSymmetrical, 2, 10, 6, BuildingSymmetrical),
-    ("west_plaza_center_flower_2024", BuildingSymmetrical, 2, 2, 6, BuildingCylindrical),
-]:
-    if "flower" not in name:
-        gs = named_grounds[(name,)]
-        ps = []
-    else:
-        gs = named_grounds[("west_plaza_center",)]
-        v = LazyVoxel(
-            name,
-            prefix=".cache/render/station/dovemere_2018/plaza",
-            voxel_getter=lambda path=f"station/voxels/dovemere_2018/plaza/{name}.vox": path,
-            load_from="station/files/cns-gorender.json",
-        )
-        v = v.discard_layers(("snow",), "nosnow")
-        ground = v.mask_clip_away("station/voxels/dovemere_2018/masks/object_ground_mask.vox", "ground")
-        ground.in_place_subset(sym.render_indices())
-        groundsprite = sym.create_variants(ground.spritesheet())
-        gscs = AChildSprite(groundsprite, (0, 0))
-        gs = gs + gscs
-
-        xofs = (16 - xspan) // 2
-        yofs = (16 - yspan) // 2
-
-        v = v.mask_clip_away("station/voxels/dovemere_2018/masks/object_mask.vox", "object")
-        v.in_place_subset(osym.render_indices())
-        sprite = osym.create_variants(v.spritesheet(xdiff=xofs, xspan=xspan, ydiff=yofs, yspan=yspan))
-        ps = [AParentSprite(sprite, (yspan, xspan, height), (yofs, xofs, 0))]
-
-    layout = ALayout(gs, ps, True, category=b"\xe8\x8a\x9cZ")
-    named_layouts[(name,)] = layout
-
+def register(layout, sym):
     for cur in [layout, layout.R] if (sym is BuildingFull) else [layout]:
         if sym is BuildingSymmetrical:
             layouts = [cur, cur.R.M]
@@ -89,3 +53,71 @@ for name, sym, xspan, yspan, height, osym in [
             doc_layout=cur,
         )
         objects.append(cur_object)
+
+
+def make_ground_layout(name, sym):
+    gs = named_grounds[(name,)]
+    layout = ALayout(gs, [], True, category=b"\xe8\x8a\x9cZ")
+    named_layouts[(name, "")] = layout
+    register(layout, sym)
+
+
+make_ground_layout("west_plaza_center", BuildingSymmetrical)
+make_ground_layout("west_plaza_offcenter", BuildingFull)
+
+
+def make_object_layout(name, sym, Xspan, Yspan, xspan, yspan, height, osym=None):
+    if osym is None:
+        osym = sym
+
+    gs = named_grounds[("west_plaza_center",)]
+    v = LazyVoxel(
+        name,
+        prefix=".cache/render/station/dovemere_2018/plaza",
+        voxel_getter=lambda path=f"station/voxels/dovemere_2018/plaza/{name}.vox": path,
+        load_from="station/files/cns-gorender.json",
+    )
+    v = v.discard_layers(("snow",), "nosnow")
+    ground = v.mask_clip_away("station/voxels/dovemere_2018/masks/object_ground_mask.vox", "ground")
+    ground.in_place_subset(sym.render_indices())
+    groundsprite = sym.create_variants(ground.spritesheet())
+    gscs = AChildSprite(groundsprite, (0, 0))
+    gs2 = gs + gscs
+
+    xofs = (16 - xspan) // 2
+    yofs = (16 - yspan) // 2
+    Xofs = (16 - Xspan) // 2
+    Yofs = (16 - Yspan) // 2
+
+    v = v.mask_clip_away("station/voxels/dovemere_2018/masks/object_mask.vox", "object")
+    v.in_place_subset(osym.render_indices())
+
+    sprite = osym.create_variants(v.spritesheet(xdiff=xofs, xspan=xspan, ydiff=yofs, yspan=yspan))
+
+    ps = [AParentSprite(sprite, (yspan, xspan, height), (yofs, xofs, 0))]
+    layout = ALayout(gs2, ps, True, category=b"\xe8\x8a\x9cZ")
+    named_layouts[(name, "")] = layout
+    register(layout, sym)
+
+    groundsprite2 = sym.create_variants(ground.spritesheet(xdiff=Xofs, xspan=Xspan, ydiff=Yofs, yspan=Yspan))
+    ps = [
+        AParentSprite(groundsprite2, (Yspan, Xspan, 0), (Yofs, Xofs - 8, 0)),
+        AParentSprite(sprite, (yspan, xspan, height), (yofs, xofs - 8, 0)),
+    ]
+    layout = ALayout(gs, ps, True, category=b"\xe8\x8a\x9cZ")
+    named_layouts[(name, "vertical")] = layout
+    register(layout, sym.break_y_symmetry())
+
+    ps = [
+        AParentSprite(groundsprite2, (Yspan, Xspan, 0), (Yofs - 8, Xofs, 0)),
+        AParentSprite(sprite, (yspan, xspan, height), (yofs - 8, xofs, 0)),
+    ]
+    layout = ALayout(gs, ps, True, category=b"\xe8\x8a\x9cZ")
+    named_layouts[(name, "horizontal")] = layout
+    register(layout, sym.break_x_symmetry())
+
+
+make_object_layout("west_plaza_center_flower_2021", BuildingSymmetrical, 8, 10, 4, 8, 6)
+make_object_layout("west_plaza_center_flower_2022", BuildingSymmetrical, 8, 10, 4, 8, 6)
+make_object_layout("west_plaza_center_flower_2023", BuildingSymmetrical, 6, 16, 2, 10, 6)
+make_object_layout("west_plaza_center_flower_2024", BuildingSymmetrical, 8, 12, 2, 2, 6, BuildingCylindrical)
