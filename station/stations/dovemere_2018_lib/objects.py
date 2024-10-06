7b4569d16cb13c36e85eac0ed4775067d4253259
Yi Yang @ Anteros <ahyangyi@gmail.com>
2024-10-07 03:00:06 +0800

    .....

--- station/stations/dovemere_2018_lib/objects.py
+++ station/stations/dovemere_2018_lib/objects.py
@@ -9,9 +9,11 @@ from station.lib import (
     AChildSprite,
     ALayout,
     AttrDict,
+    Registers,
 )
 from agrf.graphics.voxel import LazyVoxel
 from grfobject.lib import AObject
+from agrf.graphics.recolour import NON_RENDERABLE_COLOUR
 
 named_grounds = AttrDict(schema=("name"))
 named_layouts = AttrDict(schema=("name", "offset"))
@@ -24,6 +26,7 @@ for name, sym in [("west_plaza_center", BuildingSymmetrical), ("west_plaza_offce
         voxel_getter=lambda path=f"station/voxels/dovemere_2018/plaza/{name}.vox": path,
         load_from="station/files/cns-gorender.json",
     )
+    v.config["agrf_manual_crop"] = (0, 10)
     v.in_place_subset(sym.render_indices())
     sprite = sym.create_variants(v.spritesheet())
     gs = AGroundSprite(sprite)
@@ -36,7 +39,7 @@ def register(layout, sym):
             layouts = [cur, cur.R.M]
             num_views = 2
         else:
-            layouts = [cur, cur.T.M, cur.R.M, cur.T.R]
+            layouts = [cur, cur.T.R, cur.R.M, cur.T.M]
             num_views = 4
         cur_object = AObject(
             id=len(objects),
@@ -65,6 +68,21 @@ def make_ground_layout(name, sym):
 make_ground_layout("west_plaza_center", BuildingSymmetrical)
 make_ground_layout("west_plaza_offcenter", BuildingFull)
 
+all_layers = (
+    "",
+    "edge marker",
+    "flowers 1",
+    "flowers 2",
+    "flowers 3",
+    "flowers 4",
+    "flowers 5",
+    "houses",
+    "arcs",
+    "xiangqi pieces",
+    "basket",
+    "flowers in basket",
+)
+
 
 def make_object_layout(name, sym, Xspan, Yspan, xspan, yspan, height, osym=None):
     if osym is None:
@@ -77,12 +95,28 @@ def make_object_layout(name, sym, Xspan, Yspan, xspan, yspan, height, osym=None)
         voxel_getter=lambda path=f"station/voxels/dovemere_2018/plaza/{name}.vox": path,
         load_from="station/files/cns-gorender.json",
     )
-    v = v.discard_layers(("snow",), "nosnow")
+    snow = v.discard_layers(("ground snow",) + all_layers, "snow")
+    snow = snow.compose(v, "merge", ignore_mask=True, colour_map=NON_RENDERABLE_COLOUR)
+    snow.config["agrf_childsprite"] = (0, -11)
+    snow.in_place_subset(osym.render_indices())
+    snowsprite = osym.create_variants(snow.spritesheet())
+
+    ground_snow = v.discard_layers(("snow",) + all_layers, "ground_snow")
+    ground_snow = ground_snow.compose(v, "merge", ignore_mask=True, colour_map=NON_RENDERABLE_COLOUR)
+    ground_snow.config["agrf_childsprite"] = (0, -10)
+    ground_snow.in_place_subset(sym.render_indices())
+    ground_snowsprite = sym.create_variants(ground_snow.spritesheet())
+
+    v = v.discard_layers(("snow", "ground snow"), "nosnow")
     ground = v.mask_clip_away("station/voxels/dovemere_2018/masks/object_ground_mask.vox", "ground")
+    ground.config["agrf_manual_crop"] = (0, 10)
     ground.in_place_subset(sym.render_indices())
     groundsprite = sym.create_variants(ground.spritesheet())
+
     gscs = AChildSprite(groundsprite, (0, 0))
-    gs2 = gs + gscs
+    # ground_snowcs = AChildSprite(ground_snowsprite, (0, 0), flags={"dodraw": Registers.SNOW})
+    ground_snowcs = AChildSprite(ground_snowsprite, (0, 0))
+    gs2 = gs + gscs + ground_snowcs
 
     xofs = (16 - xspan) // 2
     yofs = (16 - yspan) // 2
@@ -90,11 +124,12 @@ def make_object_layout(name, sym, Xspan, Yspan, xspan, yspan, height, osym=None)
     Yofs = (16 - Yspan) // 2
 
     v = v.mask_clip_away("station/voxels/dovemere_2018/masks/object_mask.vox", "object")
+    v.config["agrf_manual_crop"] = (0, 11)
     v.in_place_subset(osym.render_indices())
-
     sprite = osym.create_variants(v.spritesheet(xdiff=xofs, xspan=xspan, ydiff=yofs, yspan=yspan))
+    snowcs = AChildSprite(snowsprite, (0, 0))
 
-    ps = [AParentSprite(sprite, (yspan, xspan, height), (yofs, xofs, 0))]
+    ps = [AParentSprite(sprite, (yspan, xspan, height), (yofs, xofs, 0)) + snowcs]
     layout = ALayout(gs2, ps, True, category=b"\xe8\x8a\x9cZ")
     named_layouts[(name, "")] = layout
     register(layout, sym)
@@ -102,7 +137,7 @@ def make_object_layout(name, sym, Xspan, Yspan, xspan, yspan, height, osym=None)
     groundsprite2 = sym.create_variants(ground.spritesheet(xdiff=Xofs, xspan=Xspan, ydiff=Yofs, yspan=Yspan))
     ps = [
         AParentSprite(groundsprite2, (Yspan, Xspan, 0), (Yofs, Xofs - 8, 0)),
-        AParentSprite(sprite, (yspan, xspan, height), (yofs, xofs - 8, 0)),
+        AParentSprite(sprite, (yspan, xspan, height), (yofs, xofs - 8, 0)) + snowcs,
     ]
     layout = ALayout(gs, ps, True, category=b"\xe8\x8a\x9cZ")
     named_layouts[(name, "vertical")] = layout
@@ -110,11 +145,19 @@ def make_object_layout(name, sym, Xspan, Yspan, xspan, yspan, height, osym=None)
 
     ps = [
         AParentSprite(groundsprite2, (Yspan, Xspan, 0), (Yofs - 8, Xofs, 0)),
-        AParentSprite(sprite, (yspan, xspan, height), (yofs - 8, xofs, 0)),
+        AParentSprite(sprite, (yspan, xspan, height), (yofs - 8, xofs, 0)) + snowcs,
     ]
     layout = ALayout(gs, ps, True, category=b"\xe8\x8a\x9cZ")
     named_layouts[(name, "horizontal")] = layout
     register(layout, sym.break_x_symmetry())
+
+    ps = [
+        AParentSprite(groundsprite2, (Yspan, Xspan, 0), (Yofs - 8, Xofs - 8, 0)),
+        AParentSprite(sprite, (yspan, xspan, height), (yofs - 8, xofs - 8, 0)) + snowcs,
+    ]
+    layout = ALayout(gs, ps, True, category=b"\xe8\x8a\x9cZ")
+    named_layouts[(name, "horizontal")] = layout
+    register(layout, sym.break_x_symmetry().break_y_symmetry())
 
 
 make_object_layout("west_plaza_center_flower_2021", BuildingSymmetrical, 8, 10, 4, 8, 6)
