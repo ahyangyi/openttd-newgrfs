777de5165fe73a902886cefcd74c11240fa4a912
Yi Yang @ Anteros <ahyangyi@gmail.com>
2024-10-06 18:28:43 +0800

    ...

--- agrf/lib/building/symmetry_test.py
+++ agrf/lib/building/symmetry_test.py
@@ -1,9 +1,35 @@
-from symmetry import BuildingSymmetrical, BuildingSymmetricalX, BuildingSymmetricalY
+from symmetry import (
+    BuildingFull,
+    BuildingSymmetrical,
+    BuildingSymmetricalX,
+    BuildingSymmetricalY,
+    BuildingRotational,
+    BuildingDiagonal,
+    BuildingCylindrical,
+)
 
 
 def test_identity():
-    symmetries = [BuildingSymmetrical, BuildingSymmetricalX, BuildingSymmetricalY]
+    symmetries = [
+        BuildingFull,
+        BuildingSymmetrical,
+        BuildingSymmetricalX,
+        BuildingSymmetricalY,
+        BuildingRotational,
+        BuildingDiagonal,
+        BuildingCylindrical,
+    ]
 
     for i in range(len(symmetries)):
         for j in range(i + 1, len(symmetries)):
             assert symmetries[i] is not symmetries[j]
+
+
+def test_is_symmetrical_y():
+    assert not BuildingFull.is_symmetrical_y()
+    assert BuildingSymmetrical.is_symmetrical_y()
+    assert not BuildingSymmetricalX.is_symmetrical_y()
+    assert BuildingSymmetricalY.is_symmetrical_y()
+    assert not BuildingRotational.is_symmetrical_y()
+    assert not BuildingDiagonal.is_symmetrical_y()
+    assert BuildingCylindrical.is_symmetrical_y()
