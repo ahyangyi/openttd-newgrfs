0dcd2d803a529b4b38bbcb27fddd4e449765c1c2
Yi Yang @ Anteros <ahyangyi@gmail.com>
2024-10-06 15:50:28 +0800

    ..

--- station/stations/dovemere_2018_lib/objects.py
+++ station/stations/dovemere_2018_lib/objects.py
@@ -29,12 +29,12 @@ for name, sym in [("west_plaza_center", BuildingSymmetrical)]:
     named_grounds[(name,)] = gs
 
 
-for name, sym, xspan, yspan in [
-    ("west_plaza_center", BuildingSymmetrical, 0, 0),
-    ("west_plaza_center_flower_2021", BuildingSymmetrical, 6, 10),
-    ("west_plaza_center_flower_2022", BuildingSymmetrical, 6, 8),
-    ("west_plaza_center_flower_2023", BuildingSymmetrical, 4, 10),
-    ("west_plaza_center_flower_2024", BuildingSymmetrical, 4, 4),
+for name, sym, xspan, yspan, height in [
+    ("west_plaza_center", BuildingSymmetrical, 0, 0, 0),
+    ("west_plaza_center_flower_2021", BuildingSymmetrical, 4, 8, 6),
+    ("west_plaza_center_flower_2022", BuildingSymmetrical, 4, 8, 6),
+    ("west_plaza_center_flower_2023", BuildingSymmetrical, 2, 10, 6),
+    ("west_plaza_center_flower_2024", BuildingSymmetrical, 2, 2, 6),
 ]:
     gs = named_grounds[("west_plaza_center",)]
     if name == "west_plaza_center":
@@ -59,7 +59,7 @@ for name, sym, xspan, yspan in [
         v = v.mask_clip_away("station/voxels/dovemere_2018/masks/object_mask.vox", "object")
         v.in_place_subset(sym.render_indices())
         sprite = sym.create_variants(v.spritesheet(xdiff=xofs, xspan=xspan, ydiff=yofs, yspan=yspan))
-        ps = [AParentSprite(sprite, (yspan, xspan, 12), (yofs, xofs, 0))]
+        ps = [AParentSprite(sprite, (yspan, xspan, height), (yofs, xofs, 0))]
     layout = ALayout(gs, ps, True, category=b"\xe8\x8a\x9cZ")
 
     for cur in [layout, layout.R] if (sym is BuildingFull) else [layout]:
