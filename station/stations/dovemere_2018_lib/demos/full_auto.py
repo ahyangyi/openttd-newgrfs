5bd0f49b311b0488f83437378781a2accae878f1
Yi Yang @ Anteros <ahyangyi@gmail.com>
2024-10-07 15:47:08 +0800

    simple noise (filter size 3, color range 3)

--- station/stations/dovemere_2018_lib/demos/full_auto.py
+++ station/stations/dovemere_2018_lib/demos/full_auto.py
@@ -6,18 +6,8 @@ from station.stations.dovemere_2018_lib.layouts import globalize_all, concourse_
 from station.stations.dovemere_2018_lib.flexible_stations import traversable
 from .utils import h_merge
 
-globalize_all(platform_class="concrete", shelter_class="shelter_2")
-
-row_2 = h_merge([traversable.demo_1(i, 2) for i in range(1, 5)], [[cns_side_and.T], [cns_side_and]])
-v_sep = [[concourse_tiles.none] * len(row_2[0])] * 2
-row_3 = h_merge([traversable.demo_1(i, 3) for i in range(1, 5)], [[cns_side_and.T], [cns_d], [cns_side_and]])
-row_4 = h_merge([traversable.demo_1(i, 4) for i in range(1, 5)], [[cns_side_and.T], [cns.T], [cns], [cns_side_and]])
-row_5 = h_merge(
-    [traversable.demo_1(i, 5) for i in range(1, 5)], [[cns_side_and.T], [cns_d], [cns_d], [cns_d], [cns_side_and]]
-)
-
 full_auto_demo = Demo(
     "Fully traversable automatic stations",
-    row_2 + v_sep + row_3 + v_sep + row_4 + v_sep + row_5,
+    traversable.demo_1(2, 2),
     remap=get_1cc_remap(CompanyColour.BLUE),
 )
