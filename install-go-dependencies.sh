2c6039c0498439f27bf41cea7e3248e31756fc6e
Yi Yang @ Anteros <ahyangyi@gmail.com>
2024-10-08 03:18:16 +0800

    .......

--- install-go-dependencies.sh
+++ install-go-dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export GOPATH=$PWD/gopath
-go install github.com/ahyangyi/gorender/cmd@3cf53a43
+go install github.com/ahyangyi/gorender/cmd@ad5c9c2
 mv gopath/bin/cmd gopath/bin/gorender
 go install github.com/ahyangyi/cargopositor/cmd@152f976
 mv gopath/bin/cmd gopath/bin/positor
