35f48efe80967bb90f5f9ae4782f294e83564823
Yi Yang @ Anteros <ahyangyi@gmail.com>
2024-10-27 03:35:43 +0800

    .............

--- agrf/lib/building/slope.py
+++ agrf/lib/building/slope.py
@@ -1,5 +1,5 @@
 from dataclass import dataclass
-from .symmetry import BuildingSymmetricalX, BuildingDiamond, BuildingCylindrical, BuildingDiagonalAlt
+from .symmetry import BuildingSymmetricalX, BuildingDiamond, BuildingCylindrical, BuildingDiagonalAlt, BuildingDiagonal
 
 
 @dataclass
@@ -11,3 +11,5 @@ flat = BuildingCylindrical.create_variants(SlopeType(0))
 ortho = BuildingSymmetricalX.create_variants(SlopeType(3), SlopeType(9), SlopeType(12), SlopeType(6))
 para = BuildingDiamond.create_variants(SlopeType(5), SlopeType(10))
 mono = BuildingDiagonalAlt.create_variants(SlopeType(1), SlopeType(2), SlopeType(8), SlopeType(4))
+tri = BuildingDiagonal.create_variants(SlopeType(7), SlopeType(14), SlopeType(13), SlopeType(11))
+steep = BuildingDiagonal.create_variants(SlopeType(23), SlopeType(30), SlopeType(27), SlopeType(29))
