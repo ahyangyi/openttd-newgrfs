e4f420f98408dee7e47abe65ec298510b2a129bb
Yi Yang @ Anteros <ahyangyi@gmail.com>
2024-11-11 15:07:34 +0800

    Run into alignment issues.

--- station/stations/dovemere_2018_lib/demos/west_plaza.py
+++ station/stations/dovemere_2018_lib/demos/west_plaza.py
@@ -18,7 +18,7 @@ station = h_merge([[[]] * 2, semitraversable.demo_1(5, 7)[5:], [[]] * 2], [[cns]
 west_stair_end = west_stair_end.lower_tile()
 overpass = overpass.lower_tile()
 west_stair = west_stair_narrow.lower_tile()
-west_stair_extender = west_stair_extender.lower_tile()
+west_stair_extender = west_stair_extender_narrow.lower_tile()
 roadstops = [[west_stair_end, overpass, west_stair, west_stair_extender, west_stair.R, overpass, west_stair_end.R]]
 
 # Objects
