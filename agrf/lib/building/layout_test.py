0e1d502edc822463c26585a99a0e8a573ee003e7
Yi Yang @ Anteros <ahyangyi@gmail.com>
2024-11-15 10:40:34 +0800

    (agrf) More test cases

--- agrf/lib/building/layout_test.py
+++ agrf/lib/building/layout_test.py
@@ -1,4 +1,4 @@
-from agrf.lib.building.layout import ADefaultGroundSprite
+from agrf.lib.building.layout import ADefaultGroundSprite, ALayout
 
 
 def test_default_groundsprite():
@@ -7,3 +7,20 @@ def test_default_groundsprite():
     graphics = gs1012.graphics(4, 32).to_image()
     assert graphics.shape == (127, 256, 4)
     assert graphics[64, 128, 1] == 67
+
+    m_graphics = gs1012.M.graphics(4, 32).to_image()
+    assert m_graphics.shape == (127, 256, 4)
+    assert m_graphics[64, 128, 1] == 72
+
+
+def test_layout():
+    gs1012 = ADefaultGroundSprite(1012)
+    l = ALayout(gs1012, [], True)
+
+    graphics = l.graphics(4, 32).to_image()
+    assert graphics.shape == (127, 256, 4)
+    assert graphics[64, 128, 1] == 67
+
+    m_graphics = l.M.graphics(4, 32).to_image()
+    assert m_graphics.shape == (127, 256, 4)
+    assert m_graphics[64, 128, 1] == 72
