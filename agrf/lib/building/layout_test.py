a5e2fc8afdd6ca647b548de906949da0f31a0fdd
Yi Yang @ Aquamarine <ahyangyi@gmail.com>
2024-11-15 21:14:14 +0800

    ..

--- agrf/lib/building/layout_test.py
+++ agrf/lib/building/layout_test.py
@@ -1,4 +1,5 @@
 from agrf.lib.building.layout import ADefaultGroundSprite, ALayout
+from agrf.lib.building.symmetry import BuildingSymmetrical
 
 
 def test_default_groundsprite():
@@ -12,6 +13,14 @@ def test_default_groundsprite():
     assert m_graphics.shape == (127, 256, 4)
     assert m_graphics[64, 128, 1] == 72
 
+    r_graphics = gs1012.R.graphics(4, 32).to_image()
+    assert r_graphics.shape == (127, 256, 4)
+    assert r_graphics[64, 128, 1] == 67
+
+    t_graphics = gs1012.T.graphics(4, 32).to_image()
+    assert t_graphics.shape == (127, 256, 4)
+    assert t_graphics[64, 128, 1] == 67
+
 
 def test_layout():
     gs1012 = ADefaultGroundSprite(1012)
@@ -24,3 +33,21 @@ def test_layout():
     m_graphics = l.M.graphics(4, 32).to_image()
     assert m_graphics.shape == (127, 256, 4)
     assert m_graphics[64, 128, 1] == 72
+
+    r_graphics = l.R.graphics(4, 32).to_image()
+    assert r_graphics.shape == (127, 256, 4)
+    assert r_graphics[64, 128, 1] == 67
+
+    t_graphics = l.T.graphics(4, 32).to_image()
+    assert t_graphics.shape == (127, 256, 4)
+    assert t_graphics[64, 128, 1] == 67
+
+
+def test_symmetry():
+    gs1012 = ADefaultGroundSprite(1012)
+    gs1011 = ADefaultGroundSprite(1011)
+
+    g = BuildingSymmetrical.create_variants([gs1012, gs1011])
+    assert g is g.R
+    assert g is g.T
+    assert g is g.M.M
