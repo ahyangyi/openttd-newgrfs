67894fb6198459de38f742e5a4c39c224ef0ef3c
Yi Yang @ Anteros <ahyangyi@gmail.com>
2024-12-13 11:12:29 +0800

    ......

--- station/stations/dovemere_1992.py
+++ station/stations/dovemere_1992.py
@@ -2,7 +2,7 @@ import grf
 from station.lib import (
     AStation,
     AMetaStation,
-    BuildingSpriteSheetSymmetricalX,
+    BuildingSymmetricalX,
     Demo,
     ADefaultGroundSprite,
     AParentSprite,
@@ -13,24 +13,25 @@ from agrf.graphics.voxel import LazyVoxel
 from agrf.magic import Switch
 
 
-def quickload(name, type, traversable):
+def quickload(name, sym, traversable):
     v = LazyVoxel(
         name,
         prefix="station/voxels/render/dovemere_1992",
         voxel_getter=lambda path=f"station/voxels/dovemere_1992/{name}.vox": path,
         load_from="station/files/gorender.json",
     )
-    sprite = type.create_variants(v.spritesheet())
+    v.in_place_subset(sym.render_indices())
+    sprite = sym.create_variants(v.spritesheet())
     ground = ADefaultGroundSprite(1012 if traversable else 1420)
     parent = AParentSprite(sprite, (16, 16, 48), (0, 0, 0))
-    candidates = [ALayout([ground], [parent], False)]
+    candidates = [ALayout(ground, [parent], False)]
     ret = []
     for l in candidates:
-        l = type.get_all_variants(l)
+        l = sym.get_all_variants(l)
         layouts.extend(l)
-        l = type.create_variants(l)
+        l = sym.create_variants(l)
         ret.append(l)
-        entries.extend(type.get_all_entries(l))
+        entries.extend(sym.get_all_entries(l))
 
     if len(ret) == 1:
         return ret[0]
@@ -39,11 +40,13 @@ def quickload(name, type, traversable):
 
 layouts = []
 entries = []
-(main,) = [
-    quickload(name, type, traversable) for name, type, traversable in [("main", BuildingSpriteSheetSymmetricalX, False)]
-]
+(main,) = [quickload(name, sym, traversable) for name, sym, traversable in [("main", BuildingSymmetricalX, False)]]
 
 the_station = AStation(
-    id=0x1000, translation_name="UNTRAVERSABLE", layouts=layouts, class_label=b"\xe8\x8a\x9ca", cargo_threshold=40
+    id=0x1000,
+    translation_name="PLATFORM_UNTRAVERSABLE",
+    layouts=layouts,
+    class_label=b"\xe8\x8a\x9ca",
+    cargo_threshold=40,
 )
-the_stations = AMetaStation([the_station], b"\xe8\x8a\x9ca", None, layouts, [Demo("Station", [[main]])])
+the_stations = AMetaStation([the_station], b"\xe8\x8a\x9ca", None, [Demo("Station", [[main]])])
