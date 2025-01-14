0ab6c1e8955e073ae6ed24e65b431410741b5873
Yi Yang @ Anteros <ahyangyi@gmail.com>
2025-01-15 00:55:04 +0800

    ..

--- station/stations/dovemere_2018_lib/flexible_stations/common.py
+++ station/stations/dovemere_2018_lib/flexible_stations/common.py
@@ -119,6 +119,12 @@ def make_central_row(l, r, suffix, fallback_suffix=None):
 def determine_platform_odd(t, d):
     if d > t:
         return {"f": "n", "n": "f", "d": "d"}[determine_platform_odd(d, t)]
+    if (t, d) == (15, 15):
+        return "d"
+    if (t, d) == (15, 14):
+        return "f"
+    if (t, d) == (15, 13):
+        return "n"
     if (t + d) % 2 == 1:
         return "fn"[d % 2]
     if (t + d) % 4 == 0:
@@ -133,6 +139,12 @@ def determine_platform_odd(t, d):
 def determine_platform_even(t, d):
     if d > t:
         return {"f": "n", "n": "f", "d": "d"}[determine_platform_even(d, t)]
+    if (t, d) == (15, 15):
+        return "d"
+    if (t, d) == (15, 14):
+        return "d"
+    if (t, d) == (15, 13):
+        return "f"
     if (t + d) % 2 == 1:
         return "nf"[d % 2]
     if (t + d) % 4 == 0:
