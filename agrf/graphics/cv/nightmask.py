8e1752a9833339369c2e05b2dce908f3683fb124
Yi Yang @ Aquamarine <ahyangyi@gmail.com>
2025-01-29 22:01:47 +0800

    ///////////

--- agrf/graphics/cv/nightmask.py
+++ agrf/graphics/cv/nightmask.py
@@ -6,7 +6,6 @@ THIS_FILE = grf.PythonFile(__file__)
 
 
 def make_night_mask(img: LayeredImage, darkness=0.75) -> LayeredImage:
-    assert img.alpha is not None
     if img.alpha is None:
         mask = ((img.mask > 0) * 255 * darkness).astype(np.uint8)
     else:
