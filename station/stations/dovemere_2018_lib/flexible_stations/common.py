eb89f23967642596659852767477a2b222992c37
Yi Yang @ Aquamarine <ahyangyi@gmail.com>
2025-02-02 16:00:04 +0800

    .......

--- station/stations/dovemere_2018_lib/flexible_stations/common.py
+++ station/stations/dovemere_2018_lib/flexible_stations/common.py
@@ -67,7 +67,7 @@ def make_row(onetile, twotile, lwall, general, window, window_extender, threetil
     )
 
 
-def make_front_row(suffix, fallback_suffix=None):
+def make_front_row(suffix):
     row = [
         named_tiles[(c, *suffix)]
         for c in ["v_end_gate", "corner_gate", "corner", "front_normal", "front_gate", "front_gate_extender"]
@@ -77,8 +77,7 @@ def make_front_row(suffix, fallback_suffix=None):
     return make_row(*row)
 
 
-def get_tile(name, desc, fallback_suffix=None):
-    # FIXME Remove fallback
+def get_tile(name, desc):
     return named_tiles[(name, *desc)]
 
 
@@ -92,40 +91,40 @@ def get_left_index_suffix(t, d, suffix, fallback_suffix=None):
     if d > t:
         return get_left_index_suffix(d, t, reverse(suffix), reverse(fallback_suffix)).T
     if t + d == 2:
-        return get_tile("side_a2", suffix, fallback_suffix)
+        return get_tile("side_a2", suffix)
     if t + d == 3:
-        return get_tile("side_a3", suffix, fallback_suffix)
+        return get_tile("side_a3", suffix)
     if t + d == 4:
-        return [get_tile("side_a", suffix, fallback_suffix), get_tile("side_b2", suffix, fallback_suffix)][d - 1]
+        return [get_tile("side_a", suffix), get_tile("side_b2", suffix)][d - 1]
     if t == d:
-        return get_tile("side_c", suffix, fallback_suffix)
+        return get_tile("side_c", suffix)
     if d == 1:
-        return get_tile("side_a", suffix, fallback_suffix)
+        return get_tile("side_a", suffix)
     if d == 2:
-        return get_tile("side_b", suffix, fallback_suffix)
-    return get_tile("side_c", suffix, fallback_suffix)
+        return get_tile("side_b", suffix)
+    return get_tile("side_c", suffix)
 
 
 def get_left_index_suffix_2(t, d, suffix, fallback_suffix):
     if t == d == 1:
-        return get_tile("side_a2_windowed", suffix, fallback_suffix)
+        return get_tile("side_a2_windowed", suffix)
     if d == 1:
-        return get_tile("side_a3_windowed", suffix, fallback_suffix)
+        return get_tile("side_a3_windowed", suffix)
     if t == 1:
-        return get_tile("side_a3_windowed", reverse(suffix), reverse(fallback_suffix)).T
-    return get_tile("side_d", suffix, fallback_suffix)
+        return get_tile("side_a3_windowed", reverse(suffix)).T
+    return get_tile("side_d", suffix)
 
 
 def make_central_row(l, r, suffix, fallback_suffix=None):
     return horizontal_layout(
         l,
         r,
-        make_vertical_switch(lambda t, d: get_tile("v_central", suffix, fallback_suffix)),
+        make_vertical_switch(lambda t, d: get_tile("v_central", suffix)),
         make_vertical_switch(lambda t, d: get_left_index_suffix_2(t, d, suffix, fallback_suffix)),
         make_vertical_switch(lambda t, d: get_left_index_suffix(t, d, suffix, fallback_suffix)),
-        make_vertical_switch(lambda t, d: get_tile("central", suffix, fallback_suffix)),
-        make_vertical_switch(lambda t, d: get_tile("central_windowed", suffix, fallback_suffix)),
-        make_vertical_switch(lambda t, d: get_tile("central_windowed_extender", suffix, fallback_suffix)),
+        make_vertical_switch(lambda t, d: get_tile("central", suffix)),
+        make_vertical_switch(lambda t, d: get_tile("central_windowed", suffix)),
+        make_vertical_switch(lambda t, d: get_tile("central_windowed_extender", suffix)),
     )
 
 
