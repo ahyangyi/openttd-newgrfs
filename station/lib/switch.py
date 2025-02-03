2e517bfdca001120a56e5c781b571492a25d0922
Yi Yang @ Anteros <ahyangyi@gmail.com>
2025-02-03 18:55:36 +0800

    ...

--- station/lib/switch.py
+++ station/lib/switch.py
@@ -1,7 +1,5 @@
-import json
-import hashlib
 import functools
-from agrf.magic import Switch
+from agrf.global_cache import make_switch
 from agrf.lib.building.layout import ALayout
 
 
@@ -31,32 +29,6 @@ def find_default_element(d):
 
     max_count = max(value_count.values())
     return [reverse_lookup.get(v, v) for v, c in value_count.items() if c == max_count][0]
-
-
-switch_cache = {}
-
-
-def switch_fingerprint(s):
-    if isinstance(s, int):
-        return s
-    return {
-        "ranges": list(sorted([(r.low, r.high, switch_fingerprint(r.ref)) for r in s._ranges])),
-        "default": switch_fingerprint(s.default),
-        "code": s.code,
-    }
-
-
-def switch_hash(s):
-    return hashlib.sha384(json.dumps(switch_fingerprint(s), sort_keys=True).encode()).hexdigest()
-
-
-def make_switch(ranges, default, code):
-    ret = Switch(ranges=ranges, default=default, code=code)
-    h = switch_hash(ret)
-    if h in switch_cache:
-        return switch_cache[h]
-    switch_cache[h] = ret
-    return ret
 
 
 class StationTileSwitch:
