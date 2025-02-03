110871403abbd928d650323c9f6b46ffd2df3e6e
Yi Yang @ Anteros <ahyangyi@gmail.com>
2025-02-03 09:45:08 +0800

    .....

--- station/lib/switch_test.py
+++ station/lib/switch_test.py
@@ -1,3 +1,4 @@
+import grf
 from .switch import make_horizontal_switch
 
 
@@ -23,3 +24,19 @@ def test_horizontal_switch():
     assert 0x120 == a.lookup(4, 4, 1, 0)
     assert 0x32 == a.T.lookup(4, 4, 0, 0)
     assert 0x301 == a.R.lookup(4, 4, 0, 0)
+
+
+def test_switch_compression():
+    a = make_horizontal_switch(lambda l, r: l)
+    a = a.to_index(None)
+    assert isinstance(a, grf.Switch)
+    assert len(a._ranges) == 15
+    assert all(isinstance(v.ref, int) for v in a._ranges)
+
+
+def test_switch_compression_2():
+    a = make_horizontal_switch(lambda l, r: r)
+    a = a.to_index(None)
+    assert isinstance(a, grf.Switch)
+    assert len(a._ranges) == 15
+    assert all(isinstance(v.ref, int) for v in a._ranges)
