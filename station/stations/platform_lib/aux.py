e4ffea23e8a325f0722a8c29433b6ac108565509
Yi Yang @ Anteros <ahyangyi@gmail.com>
2025-02-08 03:34:08 +0800

    ......

--- station/stations/platform_lib/aux.py
+++ station/stations/platform_lib/aux.py
@@ -1,4 +1,5 @@
-from station.lib import AttrDict, AParentSprite, BuildingFull
+from agrf.graphics.recolour import NON_RENDERABLE_COLOUR
+from station.lib import AttrDict, AParentSprite, AChildSprite, BuildingFull, Registers
 from agrf.graphics.voxel import LazyVoxel
 
 aux_ps = AttrDict(schema=("name",))
@@ -10,8 +11,20 @@ v = LazyVoxel(
     load_from="station/files/cns-gorender.json",
 )
 symmetry = BuildingFull
-v.in_place_subset(symmetry.render_indices())
-s = symmetry.create_variants(v.spritesheet(xspan=6, xdiff=10, yspan=6, ydiff=5))
-ps = AParentSprite(s, (6, 6, 4), (5, 10, 0)).M.R
+nosnow = v.discard_layers(("snow",), "nosnow")
+snow = v.keep_layers(("snow",), "snow")
+snow = snow.compose(v, "merge", ignore_mask=True, colour_map=NON_RENDERABLE_COLOUR)
+snow.config["overlap"] = 1.3
 
-aux_ps[("bufferstop",)] = bufferstop = ps
+nosnow.in_place_subset(symmetry.render_indices())
+nosnow.config["agrf_manual_crop"] = (0, 20)
+nosnow_sprite = symmetry.create_variants(nosnow.spritesheet(xspan=6, xdiff=10, yspan=6, ydiff=5))
+
+snow.in_place_subset(symmetry.render_indices())
+snow.config["agrf_childsprite"] = (0, -20)
+snow_sprite = symmetry.create_variants(snow.spritesheet(xspan=6, xdiff=10, yspan=6, ydiff=5))
+
+ps = AParentSprite(nosnow_sprite, (6, 6, 4), (5, 10, 0)).M.R
+cs = AChildSprite(snow_sprite, (0, 0), flags={"dodraw": Registers.SNOW}).M.R
+
+aux_ps[("bufferstop",)] = bufferstop = ps + cs
