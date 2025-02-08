e61ccf3dcda8d9482a6f2c87a1c141af7e314473
Yi Yang @ Aquamarine <ahyangyi@gmail.com>
2025-02-08 20:21:46 +0800

    (station) Inherit & modify AGRF default registers
    This style is currently used in multiple branches but not the main
    branch, potentially leading to extra merging efforts in the future.
    
    Hence, switching the main branch to the common new style makes my future
    life easier (hopefully).

--- station/lib/registers.py
+++ station/lib/registers.py
@@ -1,1 +1,8 @@
-from agrf.lib.building.registers import Registers, code
+from agrf.lib.building.registers import Registers as AGRFRegisters, code as AGRFcode
+
+
+class Registers(AGRFRegisters):
+    pass
+
+
+code = AGRFCode
