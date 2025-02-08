c6a5d329ac9291066cbedaf2afb3b344b659c2ce
Yi Yang @ Aquamarine <ahyangyi@gmail.com>
2025-02-08 20:23:49 +0800

    (station) Quick fix, previous commit obviously doesn't compile

--- station/lib/registers.py
+++ station/lib/registers.py
@@ -1,8 +1,8 @@
-from agrf.lib.building.registers import Registers as AGRFRegisters, code as AGRFcode
+from agrf.lib.building.registers import Registers as AGRFRegisters, code as agrf_code
 
 
 class Registers(AGRFRegisters):
     pass
 
 
-code = AGRFCode
+code = agrf_code
