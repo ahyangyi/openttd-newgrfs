65ab3d54080248195fb42bf348e0675219b29c9b
Yi Yang @ Anteros <ahyangyi@gmail.com>
2025-02-16 22:03:05 +0800

    (station) Move different demo groups into different sub-directories

---
+++ station/stations/dovemere_2018_lib/demos/plaza/__init__.py
@@ -0,0 +1,3 @@
+from .west_plaza import west_plaza
+
+plaza_demos = [west_plaza]
--- station/stations/dovemere_2018_lib/demos/template/side_auto.py
+++ station/stations/dovemere_2018_lib/demos/template/side_auto.py
@@ -2,17 +2,17 @@ from station.lib import Demo
 from station.stations.dovemere_2018_lib.layouts import globalize_all, concourse_tiles
 from station.lib.utils import get_1cc_remap
 from agrf.graphics.palette import CompanyColour
-from station.stations.dovemere_2018_lib.flexible_stations import side_third
-from .utils import h_merge
+from station.stations.dovemere_2018_lib.flexible_stations import side
+from ..utils import h_merge
 
 globalize_all(platform_class="concrete", shelter_class="shelter_2")
 
-station_building = h_merge([side_third.side_third_station_demo(i, 1) for i in range(1, 5)], [[cns_side_and]])[0]
+station_building = h_merge([side.side_station_demo(i, 1) for i in range(1, 5)], [[concourse.T]])[0]
 n = len(station_building)
-station_building_2 = h_merge([side_third.side_third_station_demo(i, 1) for i in [5, 7]], [[cns_side_and]])[0]
+station_building_2 = h_merge([side.side_station_demo(i, 1) for i in [5, 7]], [[concourse.T]])[0]
 assert n == len(station_building_2)
 
-side_third_auto_demo = Demo(
+side_auto_demo = Demo(
     [
         [x.T for x in station_building],
         [cns.T] * n,
@@ -25,6 +25,6 @@ side_third_auto_demo = Demo(
         [cns] * n,
         station_building_2,
     ],
-    "Traversable automatic stations",
+    "Nontraversable automatic stations",
     remap=get_1cc_remap(CompanyColour.BLUE),
 )
