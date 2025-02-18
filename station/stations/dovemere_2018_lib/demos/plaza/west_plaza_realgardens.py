15fd4224b1060b3c9f700ec5d01e13eb95ec5f43
Yi Yang @ Anteros <ahyangyi@gmail.com>
2025-02-18 11:42:26 +0800

    ........

--- station/stations/dovemere_2018_lib/demos/plaza/west_plaza_realgardens.py
+++ station/stations/dovemere_2018_lib/demos/plaza/west_plaza_realgardens.py
@@ -31,12 +31,35 @@ def real(x, y):
 
 ground = real(315, 129).lower_tile()
 crossroads = real(680, 129).lower_tile()
-grassy = real(490, 185).lower_tile()
+grassy = real(475, 143).lower_tile()
 yard = real(576, 129).lower_tile()
+path = real(560, 129).lower_tile()
+pavilion_L = real(626, 157).lower_tile()
+pavilion_R = real(624, 156).lower_tile()
+tee_L = real(568, 208).lower_tile()
+tee_R = real(570, 208).lower_tile()
+big_tee = real(582, 188).lower_tile()
+zig_L = real(648, 232).lower_tile()
+zig_R = real(649, 197).lower_tile()
+zag_L = real(647, 246).lower_tile()
+zag_R = real(650, 240).lower_tile()
+corner_L = real(640, 180).lower_tile()
+corner_R = real(634, 253).lower_tile()
+corner_TL = real(633, 240).lower_tile()
+corner_TR = real(639, 253).lower_tile()
+turn_L = real(585, 210).lower_tile()
+turn_R = real(584, 241).lower_tile()
+
+
+edge = real(615, 205).lower_tile()
+edge_bottom = real(617, 241).lower_tile()
+
+
 west_square = [
-    [ground, ground, ground, yard, ground, ground, ground],
-    [ground, ground, ground, crossroads, ground, ground, ground],
     [ground, grassy, ground, ground, ground, grassy, ground],
+    [zag_L, edge_bottom, corner_TL, path, corner_TR, edge_bottom, zag_R],
+    [corner_L, big_tee, zig_L, crossroads, zig_R, big_tee, corner_R],
+    [pavilion_L, tee_L, grassy, path, grassy, tee_R, pavilion_R],
 ]
 
 
